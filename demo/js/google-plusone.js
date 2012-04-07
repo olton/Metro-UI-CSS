89b9712ba59fbcb028482698ea2487b9b0af6080
olton-pc <sergey@pimenov.com.ua>
2012-04-07 20:37:47 +0300

    add new demo

---
+++ demo/js/google-plusone.js
@@ -0,0 +1,11 @@
+// Поместите этот тег туда, где должна отображаться кнопка +1.
+//<div class="g-plusone" data-annotation="inline"></div>
+
+
+window.___gcfg = {lang: 'ru'};
+
+(function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
---
+++ demo/js/site.js
@@ -0,0 +1,8 @@
+function switchTheme(){
+    $(".metro").toggleClass("metro-theme-light");
+    if ($(".metro").hasClass("metro-theme-light")) {
+        $('body').css("background", "#ffffff");
+    } else {
+        $('body').css("background", "#0D232E");
+    }
+}
