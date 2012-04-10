bfb9b985cfbdb01ea29135399066ad19871b88af
olton-pc <sergey@pimenov.com.ua>
2012-04-10 14:54:47 +0300

    add tiles hover effect, change demo background to dark

--- demo/js/site.js
+++ demo/js/site.js
@@ -4,6 +4,6 @@ function switchTheme(){
         $('body').css("background", "#ffffff");
     } else {
         //$('body').css("background", "#0D232E");
-        $('body').css("background", "#1d1d1d");
+        $('body').css("background", "#004050");
     }
 }
