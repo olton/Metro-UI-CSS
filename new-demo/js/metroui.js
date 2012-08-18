5266b05be64b86c06a62c2c7f53c8907257750a7
olton-pc <sergey@pimenov.com.ua>
2012-08-18 21:19:12 +0300

    Init support iPad in demo

--- new-demo/js/metroui.js
+++ new-demo/js/metroui.js
@@ -15,6 +15,9 @@
         plugin.init = function() {
             plugin.settings = $.extend({}, defaults, options);
             // code goes here
+
+            initTouchEvents();
+
             setScrollable();
             setCommands();
             setDropdown();
@@ -27,6 +30,43 @@
             // code goes here
         }
         */
+
+        var initTouchEvents = function(){
+            var src = document;
+            src.addEventListener("touchstart", touchHandler, false);
+            src.addEventListener("touchmove", touchHandler, false);
+            src.addEventListener("touchend", touchHandler, false);
+            src.addEventListener("touchcancel", touchHandler, false);
+        }
+
+        var touchHandler = function(event)
+        {
+            if (event.touches.length > 1) return;
+
+            var touches = event.changedTouches,
+                first = touches[0],
+                type = "";
+            switch(event.type)
+            {
+                case "touchstart": type = "mousedown"; break;
+                case "touchmove":  type="mousemove"; break;
+                case "touchend":   type="click"; break;
+                default: return;
+            }
+
+            //initMouseEvent(type, canBubble, cancelable, view, clickCount,
+            //           screenX, screenY, clientX, clientY, ctrlKey,
+            //           altKey, shiftKey, metaKey, button, relatedTarget);
+
+            var simulatedEvent = document.createEvent("MouseEvent");
+            simulatedEvent.initMouseEvent(type, true, true, window, 1,
+                first.screenX, first.screenY,
+                first.clientX, first.clientY, false,
+                false, false, false, 0/*left*/, null);
+
+            first.target.dispatchEvent(simulatedEvent);
+            event.preventDefault();
+        }
 
         var setSelectable = function(){
             $(".selectable").each(function(){
@@ -142,3 +182,4 @@
     }
 
 })(jQuery);
+
--- new-demo/js/site.js
+++ new-demo/js/site.js
@@ -32,3 +32,4 @@ $(window).mouseout(function (event) {
     }
 });
 */
+
