76ef69dbd7c426c7568d309dfea6600976373649
Sergey Pimenov <sergey@pimenov.com.ua>
2012-10-02 22:51:54 +0300

    A new version

--- demo/js/site.js
+++
@@ -1,9 +0,0 @@
-function switchTheme(){
-    $(".metro").toggleClass("metro-theme-light");
-    if ($(".metro").hasClass("metro-theme-light")) {
-        $('body').css("background", "#ffffff");
-    } else {
-        //$('body').css("background", "#0D232E");
-        $('body').css("background", "#004050");
-    }
-}
