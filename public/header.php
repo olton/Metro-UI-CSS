28f783cf3b234ab3dd87c38f68edf1a013706bcd
Sergey Pimenov <sergey@pimenov.com.ua>
2012-10-24 13:22:44 +0300

    update version to 0.1.4

--- public/header.php
+++ public/header.php
@@ -27,7 +27,7 @@
 
         <div class="brand">
             <a href="/"><span class="metro-ui-logo place-left"></span></a>
-            <a href="/"><span class="name">Metro UI CSS <sup class="fg-color-yellow tertiary-info-secondary-text"> v 0.1.3</sup></span></a>
+            <a href="/"><span class="name">Metro UI CSS <sup class="fg-color-yellow tertiary-info-secondary-text"> v 0.1.4</sup></span></a>
         </div>
 
         <ul>
