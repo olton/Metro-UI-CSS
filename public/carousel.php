0baf8abca26a9617d3f4f7706644925eded21b47
Sergey Pimenov <sergey@pimenov.com.ua>
2012-11-04 19:05:03 +0200

    update carousel demo

--- public/carousel.php
+++ public/carousel.php
@@ -37,21 +37,42 @@
                 <div class="grid">
                     <div class="row">
                         <div class="span5">
-                            <div class="carousel span5" style="height: 300px;" data-role="carousel" data-param-effect="slowdown" data-param-direction="left" data-param-duration="1500" data-param-period="4000">
+                            <div class="carousel span5" style="height: 300px;" data-role="carousel" data-param-effect="slide" data-param-direction="left" data-param-duration="2000" data-param-period="4000">
                                 <div class="slides">
                                     <div class="slide image" id="slide1">
                                         <img src="images/1.jpg" />
-                                        <div class="description">
-                                            Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                                        </div>
                                     </div>
                                     <div class="slide image" id="slide2">
                                         <img src="images/2.jpg" />
-                                        <div class="description">
-                                            Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                                        </div>
                                     </div>
                                     <div class="slide image" id="slide3">
+                                        <img src="images/3.jpg" />
+                                    </div>
+                                </div>
+
+
+                                <span class="control left bg-color-blue">&#8249;</span>
+                                <span class="control right bg-color-blue">&#8250;</span>
+
+                            </div>
+                        </div>
+
+                        <div class="span5">
+                            <div class="carousel span5" style="height: 300px;" data-role="carousel" data-param-effect="fade" data-param-direction="left" data-param-period="3000" data-param-markers="off">
+                                <div class="slides">
+                                    <div class="slide image" id="slide4">
+                                        <img src="images/1.jpg" />
+                                        <div class="description">
+                                            Ut egestas tempor magna, sed rutrum felis fermentum quis. Aliquam congue ultricies elit, sit amet tempor tortor tempor nec.
+                                        </div>
+                                    </div>
+                                    <div class="slide image" id="slide5">
+                                        <img src="images/2.jpg" />
+                                        <div class="description">
+                                            Ut egestas tempor magna, sed rutrum felis fermentum quis. Aliquam congue ultricies elit, sit amet tempor tortor tempor nec.
+                                        </div>
+                                    </div>
+                                    <div class="slide image" id="slide6">
                                         <img src="images/3.jpg" />
                                         <div class="description">
                                             This super beast eats fresh human flesh. She claws picking his nose, uttering at the same contented purring, and tooth picks out of leftover meat.
@@ -65,33 +86,41 @@
 
                             </div>
                         </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="span5">
+                            <div class="carousel span5" style="height: 300px;" data-role="carousel" data-param-effect="switch" data-param-direction="left" data-param-duration="300" data-param-period="4000" data-param-markers="off">
+                                <div class="slides">
+                                    <div class="slide image" id="slide7">
+                                        <img src="images/1.jpg" />
+                                    </div>
+                                    <div class="slide image" id="slide8">
+                                        <img src="images/2.jpg" />
+                                    </div>
+                                    <div class="slide image" id="slide9">
+                                        <img src="images/3.jpg" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
                         <div class="span5">
-                            <div class="carousel span5" style="height: 300px;" data-role="carousel" data-param-effect="fade" data-param-direction="left" data-param-period="3000" data-param-markers="off">
+                            <div class="carousel span5" style="height: 300px;" data-role="carousel" data-param-effect="slowdown" data-param-direction="left" data-param-period="3000" data-param-markers="off">
                                 <div class="slides">
-                                    <div class="slide image" id="slide11">
+                                    <div class="slide image" id="slide10">
                                         <img src="images/1.jpg" />
-                                        <div class="description">
-                                            Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                                        </div>
                                     </div>
-                                    <div class="slide image" id="slide21">
+                                    <div class="slide image" id="slide11">
                                         <img src="images/2.jpg" />
-                                        <div class="description">
-                                            Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-                                        </div>
                                     </div>
-                                    <div class="slide image" id="slide31">
+                                    <div class="slide image" id="slide12">
                                         <img src="images/3.jpg" />
-                                        <div class="description">
-                                            This super beast eats fresh human flesh. She claws picking his nose, uttering at the same contented purring, and tooth picks out of leftover meat.
-                                        </div>
                                     </div>
                                 </div>
 
-
-                                <span class="control left">&#8249;</span>
-                                <span class="control right">&#8250;</span>
+                                <span class="control left bg-color-darken">&#8249;</span>
+                                <span class="control right bg-color-darken">&#8250;</span>
 
                             </div>
                         </div>
@@ -137,7 +166,7 @@
                     <li><strong>period</strong> - slide change period (default: 6000)</li>
                     <li><strong>duration</strong> - effect duration period (default: 1000)</li>
                     <li><strong>effect</strong> - animation effect. available: <em>slide, fade, slowdown, switch</em> (default: slide)</li>
-                    <li><strong>direction</strong> - animation direction (default: right)</li>
+                    <li><strong>direction</strong> - animation direction. available: <em>left, right</em> (default: left)</li>
                     <li><strong>markers</strong> - on|off slide markers (default: on)</li>
                     <li><strong>arrows</strong> - on|off slide arrows (default: on)</li>
                     <li><strong>stop</strong> - on|off slide animation on mouse over (default: on)</li>
