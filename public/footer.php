00a751cce57ade636711ee14312c2e43a15beacb
Gustavo Novaro <gnovaro@gmail.com>
2013-02-10 20:14:27 -0200

    Update public/footer.php
    Minor changes, removing type, change double quotes for singles

--- public/footer.php
+++ public/footer.php
@@ -2,31 +2,31 @@
         <div class="nav-bar">
             <div class="nav-bar-inner padding10">
                 <span class="element">
-                    2012, Metro UI CSS &copy; by <a class="fg-color-white" href="mailto:sergey@pimenov.com.ua">Sergey Pimenov</a>
+                    2012 - <?php echo date('Y');?>, Metro UI CSS &copy; by <a class="fg-color-white" href="mailto:sergey@pimenov.com.ua">Sergey Pimenov</a>
                 </span>
             </div>
         </div>
     </div>
 
-    <?php include("counter.php");?>
+    <?php include('counter.php');?>
 
-    <script type="text/javascript" src="js/assets/jquery-1.8.2.min.js"></script>
-    <script type="text/javascript" src="js/assets/jquery.mousewheel.min.js"></script>
+    <script src="js/assets/jquery-1.8.2.min.js"></script>
+    <script src="js/assets/jquery.mousewheel.min.js"></script>
 
-    <script type="text/javascript" src="js/modern/dropdown.js"></script>
-    <script type="text/javascript" src="js/modern/accordion.js"></script>
-    <script type="text/javascript" src="js/modern/buttonset.js"></script>
-    <script type="text/javascript" src="js/modern/carousel.js"></script>
-    <script type="text/javascript" src="js/modern/input-control.js"></script>
-    <script type="text/javascript" src="js/modern/pagecontrol.js"></script>
-    <script type="text/javascript" src="js/modern/rating.js"></script>
-    <script type="text/javascript" src="js/modern/slider.js"></script>
-    <script type="text/javascript" src="js/modern/tile-slider.js"></script>
-    <script type="text/javascript" src="js/modern/tile-drag.js"></script>
+    <script src="js/modern/dropdown.js"></script>
+    <script src="js/modern/accordion.js"></script>
+    <script src="js/modern/buttonset.js"></script>
+    <script src="js/modern/carousel.js"></script>
+    <script src="js/modern/input-control.js"></script>
+    <script src="js/modern/pagecontrol.js"></script>
+    <script src="js/modern/rating.js"></script>
+    <script src="js/modern/slider.js"></script>
+    <script src="js/modern/tile-slider.js"></script>
+    <script src="js/modern/tile-drag.js"></script>
 
-    <script type="text/javascript" src="js/assets/github.info.js"></script>
-    <script type="text/javascript" src="js/assets/google-analytics.js"></script>
-    <script type="text/javascript" src="js/google-code-prettify/prettify.js"></script>
+    <script src="js/assets/github.info.js"></script>
+    <script src="js/assets/google-analytics.js"></script>
+    <script src="js/google-code-prettify/prettify.js"></script>
     <script src="js/sharrre/jquery.sharrre-1.3.4.min.js"></script>
 
     <script>
