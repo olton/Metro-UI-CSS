5d7eab5ace3c6c925bf7b9b5b0cb5fb995044ace
olton <sergey@pimenov.com.ua>
2013-03-08 12:51:10 +0200

    init datepicker component

--- public/calendar.php
+++ public/calendar.php
@@ -10,8 +10,27 @@
 
         <div class="page-region">
             <div class="page-region-content">
+                <h2>Calendar</h2>
                 <p>This component in progress...</p>
                 <div class="calendar span4" data-role="calendar"></div>
+<pre class="prettyprint linenums">
+    &lt;div class="<b>calnedar</b>" data-role="<b>calendar</b>"&gt;&lt;/div&gt;
+</pre>
+
+
+                <h2>Date Picker</h2>
+                <p>This component in progress...</p>
+                <div class="input-control text datepicker span4">
+                    <input type="text" />
+                    <button class="btn-date"></button>
+                </div>
+<pre class="prettyprint linenums">
+    &lt;div class="input-control text <b>datepicker</b>" data-role="<b>datepicker</b>"&gt;
+        &lt;input type="text" /&gt;
+        &lt;button class="btn-date"&gt;&lt;/button&gt;
+    &lt;/div&gt;
+</pre>
+
             </div>
         </div>
     </div>
