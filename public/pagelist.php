8a8e216e1919341d123ba63a61b142c78edd03ce
Kation <kation@outlook.com>
2013-05-31 15:33:28 +0800

    Update pagelist.php

--- public/pagelist.php
+++ public/pagelist.php
@@ -6,7 +6,6 @@
                 <a href="/" class="back-button big page-back"></a>
             </div>
         </div>
-
         <div class="page-region">
             <div class="page-region-content">
   			<div id="pageContent">This is the page of 1</div>
@@ -17,11 +16,28 @@
 						var page = $("#pageDiv").pagelist();
         				page.total = 10;
         				page.current = 1;
-        				page.url = "pagelistResult.php?page={page}";
+        				page.url = "pagelistresult.php?page={page}";
         				page.ajax = "#pageContent";
         				page.create();
 					});
                 </script>
+                <h2>Component usage</h2>
+				<pre class="prettyprint linenums span10">
+                	var page = $("#page").pagelist();
+					page.total = 10;  //Total pages
+					page.current = 1;  //Current page
+					page.showcount = 2;  //Default is 2
+					page.url = "pagelistresult.php?page={page}";  //Jump url
+					page.ajax = "#contnet";  //if you want to use ajax, set value to target element, defaul is null
+					//If you want to use your own template
+					//page.pageTemplate = '...';
+					//page.pageSideTemplate = '...';
+					//page.pageItemTemplate = '...';
+					//page.pageCurrentTemplate = '...';
+					//page.pageMoreTemplate = '...';
+					//page.pageJumpTemplate = '...';
+					page.create(); //Create page list
+				</pre>
             </div>
         </div>
     </div>
