7479297c7e6d3372a537d855b487f3c4893e5c7f
Kation <kation@outlook.com>
2013-05-31 15:35:32 +0800

    Update pagelist.php

--- public/pagelist.php
+++ public/pagelist.php
@@ -12,32 +12,32 @@
                 <div id="pageDiv"></div>
                 <script type="text/javascript" src="js/modern/pagelist.js"></script>
                 <script type="text/javascript">
-                	$(document).ready(function(){
-						var page = $("#pageDiv").pagelist();
-        				page.total = 10;
-        				page.current = 1;
-        				page.url = "pagelistresult.php?page={page}";
-        				page.ajax = "#pageContent";
-        				page.create();
-					});
+                    $(document).ready(function(){
+                        var page = $("#pageDiv").pagelist();
+                        page.total = 10;
+                        page.current = 1;
+                        page.url = "pagelistresult.php?page={page}";
+                        page.ajax = "#pageContent";
+                        page.create();
+                    });
                 </script>
                 <h2>Component usage</h2>
-				<pre class="prettyprint linenums span10">
+                <pre class="prettyprint linenums span10">
                 	var page = $("#page").pagelist();
-					page.total = 10;  //Total pages
-					page.current = 1;  //Current page
-					page.showcount = 2;  //Default is 2
-					page.url = "pagelistresult.php?page={page}";  //Jump url
-					page.ajax = "#contnet";  //if you want to use ajax, set value to target element, defaul is null
-					//If you want to use your own template
-					//page.pageTemplate = '...';
-					//page.pageSideTemplate = '...';
-					//page.pageItemTemplate = '...';
-					//page.pageCurrentTemplate = '...';
-					//page.pageMoreTemplate = '...';
-					//page.pageJumpTemplate = '...';
-					page.create(); //Create page list
-				</pre>
+                    page.total = 10;  //Total pages
+                    page.current = 1;  //Current page
+                    page.showcount = 2;  //Default is 2
+                    page.url = "pagelistresult.php?page={page}";  //Jump url
+                    page.ajax = "#contnet";  //if you want to use ajax, set value to target element, defaul is null
+                    //If you want to use your own template
+                    //page.pageTemplate = '...';
+                    //page.pageSideTemplate = '...';
+                    //page.pageItemTemplate = '...';
+                    //page.pageCurrentTemplate = '...';
+                    //page.pageMoreTemplate = '...';
+                    //page.pageJumpTemplate = '...';
+                    page.create(); //Create page list
+                </pre>
             </div>
         </div>
     </div>
