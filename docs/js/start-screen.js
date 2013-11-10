5bb017cf2caf2223693887e88474377cda82d3f9
Sergey Pimenov <sergey@pimenov.com.ua>
2013-11-10 21:59:32 +0200

    upd tiles docs

---
+++ docs/js/start-screen.js
@@ -0,0 +1,23 @@
+(function($) {
+    $.StartScreen = function(){
+        var plugin = this;
+
+        plugin.init = function(){
+            addMouseWheel();
+        };
+
+        var addMouseWheel = function (){
+            $("body").mousewheel(function(event, delta){
+                var scroll_value = delta * 50;
+                $(document).scrollLeft($(document).scrollLeft() - scroll_value);
+                return false;
+            });
+        };
+
+        plugin.init();
+    }
+})(jQuery);
+
+$(function(){
+    $.StartScreen();
+});
