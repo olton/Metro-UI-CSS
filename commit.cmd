2cd8efdc8def206428c76aa7e8e9adb4d80b44ba
Sergey Pimenov <sergey@pimenov.com.ua>
2013-12-14 14:04:35 +0200

    v 2.0.8

--- commit.cmd
+++ commit.cmd
@@ -1,25 +1,15 @@
 @ECHO = OFF
 cls
-echo "Commiting changes..."
-IF %1=="" GOTO stop
 
-echo "Copy js and css to root scope..."
 cp docs/js/metro/*.* js
 cp docs/css/metro-*.css css
 
-echo "Compress js adn css files..."
 cd compress
 node compress.js
 
-echo "Copy compressed files..."
 cd ..
 cp min/metro.min.js docs/js
 
-echo "Adding files to git"
 git add .
-echo "Commit changes"
 git commit -am %1
 
-exit 0
-
-:stop
