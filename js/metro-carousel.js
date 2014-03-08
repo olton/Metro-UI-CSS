a070844dbf41fe76d74a7db98c510b1012104c99
npbenjohnson <npbenjohnson@gmail.com>
2014-03-08 00:49:27 -0600

    carousel #178 and pt 2 of #375
    #375 fixed option usage and loading from html data- properties
    #178 Added slideIndex initial setting (set to a slide index, or 'auto' will start at 0 for direction:right and _slides.length -1 for direction:left).
    
    Added slideIndex and pageCount widget properties.
    
    Fixed issue where resize would show clipped images because they were not hidden at the end of animations.
    
    Consolidated slide and marker duplicated logic into existing _slideToSlide method to allow easy one point access from slideIndex property.

--- js/metro-carousel.js
+++ js/metro-carousel.js
@@ -1,6 +1,5 @@
-(function( $ ) {
+(function ($) {
     $.widget("metro.carousel", {
-
         version: "1.0.0",
 
         options: {
@@ -8,7 +7,7 @@
             period: 2000,
             duration: 500,
             effect: 'slowdown', // slide, fade, switch, slowdown
-            direction: 'left',
+            direction: 'right',
             markers: {
                 show: true,
                 type: 'default',
@@ -17,31 +16,41 @@
             controls: true,
             stop: true,
             width: '100%',
-            height: 300
+            height: 300,
+            slideIndex: 'auto' // if direction is left, start at end, otherwise starts at 0
         },
-
+        _getCreateOptions: function () {
+            var options = {};
+            var element = this.element;
+            if (element.data('auto') != undefined) options.auto = element.data('auto');
+            if (element.data('period') != undefined) options.period = element.data('period');
+            if (element.data('duration') != undefined) options.duration = element.data('duration');
+            if (element.data('effect') != undefined) options.effect = element.data('effect');
+            if (element.data('direction') != undefined) options.direction = element.data('direction');
+            if (element.data('width') != undefined) options.width = element.data('width');
+            if (element.data('height') != undefined) options.height = element.data('height');
+            if (element.data('stop') != undefined) options.stop = element.data('stop');
+            if (element.data('controls') != undefined) options.controls = element.data('controls');
+            if (element.data('markersShow') != undefined) options.markers.show = element.data('markersShow');
+            if (element.data('markersType') != undefined) options.markers.type = element.data('markersType');
+            if (element.data('markersPosition') != undefined) options.markers.position = element.data('markersPosition');
+            if (element.data('slideIndex') != undefined) options.slideIndex = element.data('slideIndex');
+            return options;
+        },
+        slideIndex: function (index) {
+            if (index !== undefined) {
+                this._slideIndex = this._slideToSlide(index);
+            }
+            else
+                return this._slideIndex;
+        },
+        _slideIndex: null,
+        _markersStore: null,
         _slides: {},
-        _currentIndex: 0,
         _interval: 0,
-        _outPosition: 0,
-
-        _create: function(){
-            var that = this, o = this.options,
-                element = carousel = this.element,
+        _create: function () {
+            var carousel = this.element,
                 controls = carousel.find('.controls');
-
-            if (element.data('auto') != undefined) o.auto = element.data('auto');
-            if (element.data('period') != undefined) o.period = element.data('period');
-            if (element.data('duration') != undefined) o.duration = element.data('duration');
-            if (element.data('effect') != undefined) o.effect = element.data('effect');
-            if (element.data('direction') != undefined) o.direction = element.data('direction');
-            if (element.data('width') != undefined) o.width = element.data('width');
-            if (element.data('height') != undefined) o.height = element.data('height');
-            if (element.data('stop') != undefined) o.stop = element.data('stop');
-            if (element.data('controls') != undefined) o.controls = element.data('controls');
-            if (element.data('markersShow') != undefined) o.markers.show = element.data('markersShow');
-            if (element.data('markersType') != undefined) o.markers.type = element.data('markersType');
-            if (element.data('markersPosition') != undefined) o.markers.position = element.data('markersPosition');
 
             carousel.css({
                 'width': this.options.width,
@@ -49,19 +58,20 @@
             });
 
             this._slides = carousel.find('.slide');
+            this._slides.hide();
 
             if (this._slides.length <= 1) return;
 
             if (this.options.markers !== false && this.options.markers.show && this._slides.length > 1) {
-                this._markers(that);
+                this._markers(this.options);
             }
 
             if (this.options.controls && this._slides.length > 1) {
-                carousel.find('.controls.left').on('click', function(){
-                    that._slideTo('prior');
+                carousel.find('.controls.left').on('click', this, function (evt) {
+                    evt.data.slideIndex(evt.data.slideIndex() - 1);
                 });
-                carousel.find('.controls.right').on('click', function(){
-                    that._slideTo('next');
+                carousel.find('.controls.right').on('click', this, function (evt) {
+                    evt.data.slideIndex(evt.data.slideIndex() + 1);
                 });
             } else {
                 controls.hide();
@@ -69,126 +79,108 @@
 
             if (this.options.stop) {
                 carousel
-                    .on('mouseenter', function(){
-                        clearInterval(that._interval);
+                    .on('mouseenter', this, function (evt) {
+                        clearInterval(evt.data._interval);
                     })
-                    .on('mouseleave', function(){
-                        if (that.options.auto) that._autoStart(), that.options.period;
+                    .on('mouseleave', this, function (evt) {
+                        if (evt.data.options.auto) {
+                            evt.data._autoStart();
+                        }
                     })
             }
 
             if (this.options.auto) {
                 this._autoStart();
             }
+
+            // load initial slide
+            this.slideIndex(this.options.slideIndex);
         },
 
-        _autoStart: function(){
+        _autoStart: function () {
             var that = this;
-            this._interval = setInterval(function(){
+            this._interval = setInterval(function () {
                 if (that.options.direction == 'left') {
-                    that._slideTo('next')
+                    that.slideIndex(that.slideIndex() - 1);
                 } else {
-                    that._slideTo('prior')
+                    that.slideIndex(that.slideIndex() + 1);
                 }
-            }, this.options.period);
+            }, that.options.period);
         },
 
-        _slideTo: function(direction){
-            var
-                currentSlide = this._slides[this._currentIndex],
-                nextSlide;
+        _slideToSlide: function (newIndex) {
+            // No slides or index hasn't changed
+            if (this._slides.length < 1 || newIndex === this.slideIndex()) return;
 
-            if (direction == undefined) direction = 'next';
+            if (newIndex == 'auto')
+                if (this.options.direction == 'left') {
+                    newIndex = this._slides.length - 1;
+                }
+                else {
+                    newIndex = 0;
+                }
 
-            if (direction === 'prior') {
-                this._currentIndex -= 1;
-                if (this._currentIndex < 0) this._currentIndex = this._slides.length - 1;
-
-                this._outPosition = this.element.width();
-
-            } else if (direction === 'next') {
-                this._currentIndex += 1;
-                if (this._currentIndex >= this._slides.length) this._currentIndex = 0;
-
-                this._outPosition = -this.element.width();
-
-            }
-
-            nextSlide = this._slides[this._currentIndex];
-
-            switch (this.options.effect) {
-                case 'switch': this._effectSwitch(currentSlide, nextSlide); break;
-                case 'slowdown': this._effectSlowdown(currentSlide, nextSlide, this.options.duration); break;
-                case 'fade': this._effectFade(currentSlide, nextSlide, this.options.duration); break;
-                default: this._effectSlide(currentSlide, nextSlide, this.options.duration);
-            }
-
-            var carousel = this.element, that = this;
-            carousel.find('.markers ul li a').each(function(){
-                var index = $(this).data('num');
-                if (index === that._currentIndex) {
-                    $(this).parent().addClass('active');
+            var outPosition = 0;
+            // don't calculate transition for null, it will just be a show()
+            if (this.slideIndex() !== null)
+                // If slideIndex is out of bounds continue to scroll cyclically
+                if ((newIndex > this.slideIndex() || newIndex < 0) && newIndex < this._slides.length) {
+                    outPosition = -this.element.width();
                 } else {
-                    $(this).parent().removeClass('active');
+                    outPosition = this.element.width();
                 }
-            });
-        },
 
-        _slideToSlide: function(slideIndex){
-            var
-                currentSlide = this._slides[this._currentIndex],
-                nextSlide = this._slides[slideIndex];
+            // Bring slideIndex back into bounds
+            var len = this._slides.length;
+            newIndex = ((newIndex % len) + len) % len;
 
-            if (slideIndex > this._currentIndex) {
-                this._outPosition = -this.element.width();
-            } else {
-                this._outPosition = this.element.width();
+            var nextSlide = this._slides.eq(newIndex);
+
+            if (this.slideIndex() !== null) {
+                var currentSlide = this._slides[this.slideIndex()];
+                // Apply effects
+                switch (this.options.effect) {
+                    case 'switch': this._effectSwitch(currentSlide, nextSlide, outPosition); break;
+                    case 'slowdown': this._effectSlowdown(currentSlide, nextSlide, this.options.duration, outPosition); break;
+                    case 'fade': this._effectFade(currentSlide, nextSlide, this.options.duration), outPosition; break;
+                    default: this._effectSlide(currentSlide, nextSlide, this.options.duration, outPosition);
+                }
+            }
+            else {
+                nextSlide.show();
             }
 
-            switch (this.options.effect) {
-                case 'switch' : this._effectSwitch(currentSlide, nextSlide); break;
-                case 'slowdown': this._effectSlowdown(currentSlide, nextSlide, this.options.duration); break;
-                case 'fade': this._effectFade(currentSlide, nextSlide, this.options.duration); break;
-                default : this._effectSlide(currentSlide, nextSlide, this.options.duration);
+            // Select active marker if there are markers available
+            if (this._markersStore !== null) {
+                this._markersStore.removeClass('active');
+                this._markersStore.eq(newIndex).addClass('active');
             }
 
-            this._currentIndex = slideIndex;
+            return newIndex;
         },
 
-        _markers: function (that) {
-            var div, ul, li, i, markers;
+        _markers: function () {
+            var div, ul, li;
 
-            div = $('<div class="markers '+this.options.markers.type+'" />');
+            div = $('<div class="markers ' + this.options.markers.type + '" />');
             ul = $('<ul></ul>').appendTo(div);
 
-            for (i = 0; i < this._slides.length; i++) {
-                li = $('<li><a href="javascript:void(0)" data-num="' + i + '"></a></li>');
-                if (i === 0) {
-                    li.addClass('active');
-                }
+            for (var i = 0; i < this._slides.length; i++) {
+                li = $('<li><a href="javascript:void(0)"></a></li>');
                 li.appendTo(ul);
             }
 
+            this._markersStore = ul.children();
 
-            ul.find('li a').removeClass('active').on('click', function () {
-                var $this = $(this),
-                    index = $this.data('num');
-
-                ul.find('li').removeClass('active');
-                $this.parent().addClass('active');
-
-                if (index == that._currentIndex) {
-                    return true;
-                }
-
-                that._slideToSlide(index);
+            ul.on('click', 'li', this, function (evt) {
+                evt.data.slideIndex(evt.data._markersStore.index(this));
                 return true;
             });
 
             div.appendTo(this.element);
 
             switch (this.options.markers.position) {
-                case 'top-left' : {
+                case 'top-left': {
                     div.css({
                         left: '10px',
                         right: 'auto',
@@ -197,7 +189,7 @@
                     });
                     break;
                 }
-                case 'top-right' : {
+                case 'top-right': {
                     div.css({
                         left: 'auto',
                         right: '10px',
@@ -206,32 +198,32 @@
                     });
                     break;
                 }
-                case 'top-center' : {
+                case 'top-center': {
                     div.css({
-                        left: this.element.width()/2 - div.width()/2,
+                        left: this.element.width() / 2 - div.width() / 2,
                         right: 'auto',
                         bottom: 'auto',
                         top: '0px'
                     });
                     break;
                 }
-                case 'bottom-left' : {
+                case 'bottom-left': {
                     div.css({
                         left: '10px',
                         right: 'auto'
                     });
                     break;
                 }
-                case 'bottom-right' : {
+                case 'bottom-right': {
                     div.css({
                         right: '10px',
                         left: 'auto'
                     });
                     break;
                 }
-                case 'bottom-center' : {
+                case 'bottom-center': {
                     div.css({
-                        left: this.element.width()/2 - div.width()/2,
+                        left: this.element.width() / 2 - div.width() / 2,
                         right: 'auto'
                     });
                     break;
@@ -239,66 +231,64 @@
             }
         },
 
-
-        _effectSwitch: function(currentSlide, nextSlide){
+        _effectSwitch: function (currentSlide, nextSlide) {
             $(currentSlide)
                 .hide();
             $(nextSlide)
-                .css({left: 0})
+                .css({ left: 0 })
                 .show();
         },
 
-        _effectSlide: function(currentSlide, nextSlide, duration){
+        _effectSlide: function (currentSlide, nextSlide, duration, outPosition) {
             $(currentSlide)
-                .animate({left: this._outPosition}, duration);
+                .animate(
+                { left: outPosition },
+                duration,
+                'swing',
+                function () { $(this).hide(); });
             $(nextSlide)
-                .css('left', this._outPosition * -1)
+                .css('left', outPosition * -1)
                 .show()
-                .animate({left: 0}, duration);
+                .animate({ left: 0 }, duration);
         },
 
-        _effectSlowdown: function(currentSlide, nextSlide, duration){
-            var options = {
-                'duration': duration,
-                'easing': 'doubleSqrt'
-            };
-            $.easing.doubleSqrt = function(t) {
+        _effectSlowdown: function (currentSlide, nextSlide, duration, outPosition) {
+            $.easing.doubleSqrt = function (t) {
                 return Math.sqrt(Math.sqrt(t));
             };
 
             $(currentSlide)
-                .animate({left: this._outPosition}, options);
+                .animate(
+                { left: outPosition },
+                duration,
+                'doubleSqrt',
+                function () { $(this).hide() }
+                );
 
-
-            //$(nextSlide).find('.subslide').hide();
             $(nextSlide)
-                .css('left', this._outPosition * -1)
+                .css('left', outPosition * -1)
                 .show()
-                .animate({left: 0}, options);
-
-            //setTimeout(function(){
-            //    $(nextSlide).find('.subslide').fadeIn();
-            //}, 500);
-
+                .animate(
+                { left: 0 }, duration,
+                'doubleSqrt'
+                );
         },
 
-        _effectFade: function(currentSlide, nextSlide, duration){
+        _effectFade: function (currentSlide, nextSlide, duration) {
             $(currentSlide)
                 .fadeOut(duration);
             $(nextSlide)
-                .css({left: 0})
+                .css({ left: 0 })
                 .fadeIn(duration);
         },
 
-
-        _destroy: function(){
+        _destroy: function () {
 
         },
 
-        _setOption: function(key, value){
-            this._super('_setOption', key, value);
-        }
+        _setOption: function (key, value) {
+            this._super(key, value);
+        },
+        pageCount: function () { return this._slides.length; }
     });
-})( jQuery );
-
-
+})(jQuery);
