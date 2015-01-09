7455851f65ddfeefb0dd4d2c3843a006f7eb0e5b
Sergey Pimenov <sergey@pimenov.com.ua>
2015-01-09 13:55:46 +0200

    upd wizard add customize buttons

--- docs/js/metro/metro-wizard.js
+++ docs/js/metro/metro-wizard.js
@@ -53,9 +53,8 @@
 
             this._createEvents();
 
-            if (o.startPage != 'default' && parseInt(o.startPage) > 1) {
-                this.stepTo(o.startPage);
-            }
+            var sp = (o.startPage != 'default' && parseInt(o.startPage) > 1) ? o.startPage : 1;
+            this.stepTo(sp);
 
             this.options.onPage(this._currentStep + 1, element);
 
@@ -82,29 +81,110 @@
                 var actions = $("<div/>").addClass("actions").appendTo(element);
                 var group_left = $("<div/>").addClass("group-left").appendTo(actions);
                 var group_right = $("<div/>").addClass("group-right").appendTo(actions);
+                var cancel_button, help_button, prior_button, next_button, finish_button;
 
                 if (o.buttons.cancel) {
-                    $("<button type='button'/>").addClass("btn-cancel").html($.Metro.Locale[o.locale].buttons[2]).appendTo(group_left).on('click', function(){
+                    cancel_button = $("<button type='button'/>").addClass("btn-cancel").html($.Metro.Locale[o.locale].buttons[2]);
+                    if (typeof o.buttons.cancel == "boolean") {
+                        cancel_button.appendTo(group_left);
+                    } else {
+                        if (o.buttons.cancel.title) {
+                            cancel_button.html(o.buttons.cancel.title);
+                        }
+                        if (o.buttons.cancel.cls) {
+                            cancel_button.addClass(o.buttons.cancel.cls);
+                        }
+                        if (o.buttons.cancel.group && o.buttons.cancel.group != "left") {
+                            cancel_button.appendTo(group_right);
+                        } else {
+                            cancel_button.appendTo(group_left);
+                        }
+                    }
+                    cancel_button.on('click', function(){
                         o.onCancel(that._currentStep+1, element);
                     });
                 }
                 if (o.buttons.help) {
-                    $("<button type='button'/>").addClass("btn-help").html($.Metro.Locale[o.locale].buttons[3]).appendTo(group_right).on('click', function(){
+                    help_button = $("<button type='button'/>").addClass("btn-help").html($.Metro.Locale[o.locale].buttons[3]);
+                    if (typeof o.buttons.help == "boolean") {
+                        help_button.appendTo(group_right);
+                    } else {
+                        if (o.buttons.help.title) {
+                            help_button.html(o.buttons.help.title);
+                        }
+                        if (o.buttons.help.cls) {
+                            help_button.addClass(o.buttons.help.cls);
+                        }
+                        if (o.buttons.help.group && o.buttons.help.group != "left") {
+                            help_button.appendTo(group_right);
+                        } else {
+                            help_button.appendTo(group_left);
+                        }
+                    }
+                    help_button.on('click', function(){
                         o.onHelp(that._currentStep+1, element);
                     });
                 }
                 if (o.buttons.prior) {
-                    $("<button type='button'/>").addClass("btn-prior").html($.Metro.Locale[o.locale].buttons[4]).appendTo(group_right).on('click', function(){
+                    prior_button = $("<button type='button'/>").addClass("btn-prior").html($.Metro.Locale[o.locale].buttons[4]);
+                    if (typeof o.buttons.prior == "boolean") {
+                        prior_button.appendTo(group_right);
+                    } else {
+                        if (o.buttons.prior.title) {
+                            prior_button.html(o.buttons.prior.title);
+                        }
+                        if (o.buttons.prior.cls) {
+                            prior_button.addClass(o.buttons.prior.cls);
+                        }
+                        if (o.buttons.prior.group && o.buttons.prior.group != "left") {
+                            prior_button.appendTo(group_right);
+                        } else {
+                            prior_button.appendTo(group_left);
+                        }
+                    }
+                    prior_button.on('click', function(){
                         if (o.onPrior(that._currentStep+1, element)) that.prior();
                     });
                 }
                 if (o.buttons.next) {
-                    $("<button type='button'/>").addClass("btn-next").html($.Metro.Locale[o.locale].buttons[5]).appendTo(group_right).on('click', function(){
+                    next_button = $("<button type='button'/>").addClass("btn-next").html($.Metro.Locale[o.locale].buttons[5]);
+                    if (typeof o.buttons.next == "boolean") {
+                        next_button.appendTo(group_right);
+                    } else {
+                        if (o.buttons.next.title) {
+                            next_button.html(o.buttons.next.title);
+                        }
+                        if (o.buttons.next.cls) {
+                            next_button.addClass(o.buttons.next.cls);
+                        }
+                        if (o.buttons.next.group && o.buttons.next.group != "left") {
+                            next_button.appendTo(group_right);
+                        } else {
+                            next_button.appendTo(group_left);
+                        }
+                    }
+                    next_button.on('click', function(){
                         if (o.onNext(that._currentStep+1, element)) that.next();
                     });
                 }
                 if (o.buttons.finish) {
-                    $("<button type='button' disabled/>").addClass("btn-finish").html($.Metro.Locale[o.locale].buttons[6]).appendTo(group_right).on('click', function(){
+                    finish_button = $("<button type='button'/>").addClass("btn-finish").html($.Metro.Locale[o.locale].buttons[6]);
+                    if (typeof o.buttons.finish == "boolean") {
+                        finish_button.appendTo(group_right);
+                    } else {
+                        if (o.buttons.finish.title) {
+                            finish_button.html(o.buttons.finish.title);
+                        }
+                        if (o.buttons.finish.cls) {
+                            finish_button.addClass(o.buttons.finish.cls);
+                        }
+                        if (o.buttons.finish.group && o.buttons.finish.group != "left") {
+                            finish_button.appendTo(group_right);
+                        } else {
+                            finish_button.appendTo(group_left);
+                        }
+                    }
+                    finish_button.on('click', function(){
                         o.onFinish(that._currentStep+1, element);
                     });
                 }
@@ -115,6 +195,7 @@
             var new_step = this._currentStep + 1;
 
             if (new_step == this._steps.length) return false;
+
             this._currentStep = new_step;
             this._steps.hide();
             $(this._steps[new_step]).show();
@@ -129,6 +210,16 @@
                 this.element.find('.btn-finish').attr('disabled', true);
             }
 
+            if (new_step == this._steps.length - 1) {
+                this.element.find('.btn-next').attr('disabled', true);
+            } else {
+                this.element.find('.btn-next').attr('disabled', false);
+            }
+
+            if (new_step > 0) {
+                this.element.find('.btn-prior').attr('disabled', false);
+            }
+
             return true;
         },
 
@@ -136,6 +227,7 @@
             var new_step = this._currentStep - 1;
 
             if (new_step < 0) return false;
+
             this._currentStep = new_step;
             this._steps.hide();
             $(this._steps[new_step]).show();
@@ -148,6 +240,16 @@
                 this.element.find('.btn-finish').attr('disabled', false);
             } else {
                 this.element.find('.btn-finish').attr('disabled', true);
+            }
+
+            if (new_step == 0) {
+                this.element.find('.btn-prior').attr('disabled', true);
+            } else {
+                this.element.find('.btn-prior').attr('disabled', false);
+            }
+
+            if (new_step < finish) {
+                this.element.find('.btn-next').attr('disabled', false);
             }
 
             return true;
@@ -170,6 +272,12 @@
             } else {
                 this.element.find('.btn-finish').attr('disabled', true);
             }
+
+
+            console.log(new_step, finish);
+
+            this.element.find('.btn-next').attr('disabled', !(new_step < finish));
+            this.element.find('.btn-prior').attr('disabled', !(new_step > 0));
 
             return true;
         },
