ace0304449a431e30b498f9f9a8e8ded014c41cf
Xiphe D.M. <xiphe@xiphe.com>
2015-05-11 20:12:56 +0200

    flexible: small reorganizations

--- js/widgets/appbar.js
+++ js/widgets/appbar.js
@@ -83,7 +83,7 @@
 
             //adds the left margin dedicated to the first child
             childrenWidth += parseInt($(children).first().css("margin-left"));
-            console.log("margin of left: " + childrenWidth)
+
             //walk trough the children and add the size, 
             for (var i = 0, len = children.length - 1; i <= len; i++) {
                 childrenWidth += $(children[i]).outerWidth();
@@ -125,22 +125,22 @@
 
 
                 //find out in which menubar we are located in
-                var topMenuBar = $(nextToHide).parent(); //this is only the appbar-menu-bar not the appbar itself
+                var topMenuBar = $(nextToHide).parent(); //this is only a appbar-menu not the appbar itself
                 //find out where we have to go
                 var topMenuBarIndex = $(that.flexVisibles).index($(nextToHide).parent());
                 var pullMenuBar = $(that.pullMenu).find(".app-bar-menu").eq(topMenuBarIndex); //TODO: Make the class app-bar-menu configurable - perhaps sidebar
-                
+
                 //mark the entry as a entry of the pullmenu and move it to the pullmenu
                 $(nextToHide)
                         .prependTo(pullMenuBar)
                         .addClass("app-bar-pullmenu-entry");
-                
+
                 //the menubar is initiated with the hidden class, so we do not see empty pullmenubars, we must unhide them
                 //it does not matter, if we see it already, we do it always:
                 $(pullMenuBar).removeClass("hidden");
 
                 //in case there are no more entries in the top menu bar we can hide it
-                if($(topMenuBar).children().length === 0) {
+                if ($(topMenuBar).children().length === 0) {
                     $(topMenuBar).addClass("hidden");
                 }
 
@@ -149,7 +149,7 @@
                 $(that.pullButton).show();
 
                 return nextToHide;
-                
+
             } else if (direction === "fromPullMenu") {
                 //get next candidate which could be moved to the topbar menu, in fact the first which is still marked as pullmenu-entry
                 var nextToShow = $(that.allMenuEntries).filter(".app-bar-pullmenu-entry").first();
@@ -159,14 +159,34 @@
                     return false;
                 }
 
-                //remove the mark as a entry of the pullmenu and move it to the normal standard bar
-                //$(nextToHide).removeClass("app-bar-pullmenu-entry").appendTo(mark!!);
+                //find out in which pullmenu we are located in
+                var pullMenuBar = $(nextToShow).parent(); //only one single menu, not the whole thing
+
+                //find out where we have to go
+                var topMenuBarIndex = $(pullMenuBar).index(); //it is the same structur as that.flexVisibles, so we can use the simple index
+                var topMenu = $(that.flexVisibles).eq(topMenuBarIndex);
+                //mark the entry as a entry of the pullmenu and move it to the pullmenu
+                $(nextToShow)
+                        .removeClass("app-bar-pullmenu-entry")
+                        .appendTo(topMenu);
+
+
+                //in case there are no more entries left, we can hide the pullbar menu from this entry
+                if ($(pullMenuBar).children().length === 0) {
+                    $(pullMenuBar).addClass("hidden");
+                }
+
+                //in case we have no more menus in the pullbar area, we hide the pullbar thing
+                if ($(that.pullMenu).children(".app-bar-pullmenubar").length === 0) {
+                    $(pullMenuBar).addClass("hidden");
+                }
+
+                return nextToShow;
             }
         },
         _checkMenuEntries: function () {
             var that = this, element = this.element, o = this.options;
-            var recheck = true;
-            while (recheck) {
+            while (true) {
 
                 //calculate the empty space within the appbar we can use for hidden children
                 that._calculateFreeSpace();
@@ -177,10 +197,9 @@
                 if (freeSpace < 0) {
                     //no space left, we hide a menu entry now
 
-                    //move the menu entry and check if there are more menuentries left
-                    if (!(this._moveMenuEntry("toPullMenu"))) {
+                    //move the menu entry to the pullbar and check if there are more menuentries left
+                    if (!(that._moveMenuEntry("toPullMenu"))) {
                         //nothing left to hide
-                        recheck = false;
                         break;
                     } else {
                         //we moved successfully, perhaps we can hide more entries, we recheck the appbar, 
@@ -195,23 +214,26 @@
                     //check if there is something to do
 
 
-                    break;
                 }
 
+                //we continue manually. if we reach the end of the loop we end this better so we do not produce infinite loop accidentally
+                break;
             }
         },
-        resize: function() {
+        resize: function () {
             var that = this, element = this.element, o = this.options;
-            
-            this._checkMenuEntries();
+            if (that.initiatedAsFlex) {
+                this._checkMenuEntries();
+            }
         },
         _initBar: function () {
             var that = this, element = this.element, o = this.options;
-            that.initiatedAs;
+
+            that.lastFlexAction = undefined;
             console.log("------")
             that.pullButton = $(element).find('.app-bar-pullbutton');
             var menus = $(element).find('.app-bar-menu');
-
+            that.initiatedAsFlex = false;
             var flexVisible, menuEntries; //temporarly used vars
 
             that.flexVisibles = $();    //the menus which are directly in the appbar
@@ -223,100 +245,103 @@
                 //strip off all .no-flexible menus
                 that.flexVisibles = $(menus).not(".no-flexible");
 
+                if (that.flexVisibles.length > 0) {
 
-                //sort the menus according to the data-flexorder attribute
-                that.flexVisibles.sort(function (a, b) {
-                    var aValue = (parseInt($(a).data("flexorder")) || $(a).index() + 1);
-                    var bValue = (parseInt($(b).data("flexorder")) || $(b).index() + 1);
-                    return aValue - bValue;
-                });
+                    that.initiatedAsFlex = true;
 
-                //get all children in a sorted order according to the data-flexorder attribute
-                $(that.flexVisibles).each(function () {
-                    flexVisible = this;
-
-                    menuEntries = $(flexVisible).children().not(".no-flexible")  //strip off all .no-flexible elements
-                    menuEntries.sort(function (a, b) {
+                    //sort the menus according to the data-flexorder attribute
+                    that.flexVisibles.sort(function (a, b) {
                         var aValue = (parseInt($(a).data("flexorder")) || $(a).index() + 1);
                         var bValue = (parseInt($(b).data("flexorder")) || $(b).index() + 1);
                         return aValue - bValue;
                     });
 
+                    //get all children in a sorted order according to the data-flexorder attribute
+                    $(that.flexVisibles).each(function () {
+                        flexVisible = this;
 
-                    $.merge(that.allMenuEntries, menuEntries);
-                });
-
-                //find the parent, which contains all menus
-                that.menusParent = $(element).find(".app-bar-menu").first().parent();
-
-                // === create a pull down button + pull menu ===
-                //check if a pulldown button already exists, if not we create one
-                if (!(that.pullButton.length > 0)) {
-                    //DOC: We can create a invisible button, if we want to force to not show a pull button
-                    that.pullButton = $('<div class="app-bar-pullbutton automatic"></div>');
-                    $(that.menusParent).append(that.pullButton);
-                }
-
-                //create a pullmenu 
-                that.pullMenu = $('<nav class="app-bar-pullmenu" />');
-
-                //create menubars within the pullmenu
-                that.flexVisibles.each(function () {
-                    $(that.pullMenu).append($('<ul class="app-bar-pullmenubar hidden app-bar-menu" />'));  //TODO: Make the class app-bar-menu configurable - perhaps sidebar, in that case we have to position absolute in the appbar.less
-                });
-
-
-                $(that.menusParent).append(that.pullMenu);
-
-                //check for the first time the menu entries /hide them if needed, etc.
-                that._checkMenuEntries();
-                
-                
-                
-                //===  EVENTS =================================================
-
-                //activate the click event for the pull button
-                $(that.pullButton).on("click", function() {
-                    
-                    //who am i?
-                    that = $(this).closest("[data-role=appbar]").data("appbar");
-
-                    //we show /hide the pullmenu
-                    if($(that.pullMenu).is(":hidden")) {
-                        $(that.pullMenu).show();
-                        $(that.pullMenu).find(".app-bar-pullmenubar").slideDown("fast");            //TODO: make the animation effect configurable
-                    } else {
-                        $(that.pullMenu).find(".app-bar-pullmenubar").slideUp("fast", function() {  //TODO: make the animation effect configurable
-                             $(that.pullMenu).hide();
+                        menuEntries = $(flexVisible).children().not(".no-flexible")  //strip off all .no-flexible elements
+                        menuEntries.sort(function (a, b) {
+                            var aValue = (parseInt($(a).data("flexorder")) || $(a).index() + 1);
+                            var bValue = (parseInt($(b).data("flexorder")) || $(b).index() + 1);
+                            return aValue - bValue;
                         });
+
+
+                        $.merge(that.allMenuEntries, menuEntries);
+                    });
+
+                    //find the parent, which contains all menus
+                    that.menusParent = $(element).find(".app-bar-menu").first().parent();
+
+                    // === create a pull down button + pull menu ===
+                    //check if a pulldown button already exists, if not we create one
+                    if (!(that.pullButton.length > 0)) {
+                        //DOC: We can create a invisible button, if we want to force to not show a pull button
+                        that.pullButton = $('<div class="app-bar-pullbutton automatic"></div>');
+                        $(that.menusParent).append(that.pullButton);
                     }
-                });
-                
-                
-                //we have to calculate everything new, if the user resizes or zooms the window
-                $(window).resize(function() {
-                    $("[data-role=appbar]:not(.no-flexible)").each(function() {
-                       $(this).data("appbar").resize();
+
+                    //create a pullmenu 
+                    that.pullMenu = $('<nav class="app-bar-pullmenu" />');
+
+                    //create menubars within the pullmenu
+                    that.flexVisibles.each(function () {
+                        $(that.pullMenu).append($('<ul class="app-bar-pullmenubar hidden app-bar-menu" />'));  //TODO: Make the class app-bar-menu configurable - perhaps sidebar, in that case we have to position absolute in the appbar.less
                     });
-                });
-                
-                
-                //because fonts(also icon-fonts) are often loaded async after the page has loaded and this script walked through already, 
-                //we have to check again after these elements loaded. Because there is no way to observe only specific elements, we do it for the window
-                $(window).load(function() {
-                    $("[data-role=appbar]:not(.no-flexible)").each(function() {
-                       $(this).data("appbar").resize();
+
+
+                    $(that.menusParent).append(that.pullMenu);
+
+                    //check for the first time the menu entries /hide them if needed, etc.
+                    that._checkMenuEntries();
+
+
+
+                    //===  EVENTS =================================================
+
+                    //activate the click event for the pull button
+                    $(that.pullButton).on("click", function () {
+
+                        //who am i?
+                        that = $(this).closest("[data-role=appbar]").data("appbar");
+
+                        //we show /hide the pullmenu
+                        if ($(that.pullMenu).is(":hidden")) {
+                            $(that.pullMenu).show();
+                            $(that.pullMenu).find(".app-bar-pullmenubar").slideDown("fast");            //TODO: make the animation effect configurable
+                        } else {
+                            $(that.pullMenu).find(".app-bar-pullmenubar").slideUp("fast", function () {  //TODO: make the animation effect configurable
+                                $(that.pullMenu).hide();
+                            });
+                        }
                     });
-                });
-                
-                //pictures (or other outside stuff was loaded - pictures are also often loaded async or have a lazy load or are injected after a while. 
-                //a picture can change a size of the element from the appbar, so we must recheck it again.
-                $("[data-role=appbar]:not(.no-flexible) [src]").on("load", function() {
-                    //who am i?
-                    var appbar = $(this).closest("[data-role=appbar]").data("appbar");
-                    appbar.resize();
-                });
-                
+
+
+                    //we have to calculate everything new, if the user resizes or zooms the window
+                    $(window).resize(function () {
+                        $("[data-role=appbar]:not(.no-flexible)").each(function () {
+                            $(this).data("appbar").resize();
+                        });
+                    });
+
+
+                    //because fonts(also icon-fonts) are often loaded async after the page has loaded and this script walked through already, 
+                    //we have to check again after these elements loaded. Because there is no way to observe only specific elements, we do it for the window
+                    $(window).load(function () {
+                        $("[data-role=appbar]:not(.no-flexible)").each(function () {
+                            $(this).data("appbar").resize();
+                        });
+                    });
+
+                    //pictures (or other outside stuff was loaded - pictures are also often loaded async or have a lazy load or are injected after a while. 
+                    //a picture can change a size of the element from the appbar, so we must recheck it again.
+                    $("[data-role=appbar]:not(.no-flexible) [src]").on("load", function () {
+                        //who am i?
+                        var appbar = $(this).closest("[data-role=appbar]").data("appbar");
+                        appbar.resize();
+                    });
+                }
             }
 
         },
