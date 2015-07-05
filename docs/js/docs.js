de998f7f9ef3cce44ffaee9bb82455ddccd85189
Sergey Pimenov <sergey@pimenov.com.ua>
2015-07-05 10:40:24 +0300

    fix mif-lg class

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -28,7 +28,9 @@ jQuery(document).ready(function($){
     $("h1 .nav-button").addClass('transform');
 })(jQuery);
 
+
 if (window.location.hostname !== 'localhost') {
+    console.log(window.location.hostname);
 
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
