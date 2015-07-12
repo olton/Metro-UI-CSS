f067a7ef7d0d95a73c900b8e247870027bf8c98c
Sergey Pimenov <sergey@pimenov.com.ua>
2015-07-12 22:22:42 +0300

    fucking requireJS

--- test/RequireJS/scripts/main.js
+++ test/RequireJS/scripts/main.js
@@ -2,18 +2,26 @@ require.config({
     baseUrl: "scripts",
     paths: {
         jquery: "jquery",
-        metro: "metro"
+        metro_global: "metro/global",
+        widget_factory: "metro/widget",
+        widget_initiator: "metro/initiator",
+        widget_countdown: "metro/widgets/countdown",
+        widget_accordion: "metro/widgets/accordion"
     },
     shim: {
-        metro: ["jquery"]
+        metro_global: ["jquery"],
+        widget_factory: ["jquery"],
+        widget_initiator: ["jquery"],
+        widget_countdown: ["jquery"],
+        widget_accordion: ["jquery"]
     }
 });
 
 require(
-    ['jquery', 'metro'],
+    ['jquery', 'metro_global', 'widget_factory', 'widget_initiator', 'widget_countdown', 'widget_accordion'],
     function($){
         $(function(){
-            metroInitWidgets(jQuery);
+            $.Metro.initWidgets();
         });
     }
 );
