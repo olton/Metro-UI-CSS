c694fcfd9f5db1beca8250f35b8f53e1411d6164
Sergey Pimenov <sergey@pimenov.com.ua>
2015-07-20 12:02:55 +0300

    add charm widget

---
+++ js/widgets/charm.js
@@ -0,0 +1,163 @@
+$.widget( "metro.charm" , {
+
+    version: "3.0.0",
+
+    options: {
+        position: "right",
+        opacity: 1,
+        outside: false,
+        timeout: 0,
+        duration: 400
+    },
+
+    _create: function () {
+        var that = this, element = this.element, o = this.options;
+
+        $.each(element.data(), function(key, value){
+            if (key in o) {
+                try {
+                    o[key] = $.parseJSON(value);
+                } catch (e) {
+                    o[key] = value;
+                }
+            }
+        });
+
+
+        this._createCharm();
+
+        element.data('charm', this);
+    },
+
+    _createCharm: function(){
+        var that = this, element = this.element, o = this.options;
+
+        element.addClass("charm").addClass(o.position+"-side").css({opacity: o.opacity}).hide();
+
+        var closer = $("<span/>").addClass("charm-closer").appendTo(element);
+        closer.on('click', function(){
+            that.close();
+        });
+
+        if (o.outside === true) {
+            element.on('mouseleave', function(e){
+                that.close();
+            });
+        }
+    },
+
+    _showCharm: function(){
+        var that = this, element = this.element, o = this.options;
+        var size;
+
+        if (o.position === "left" || o.position === "right") {
+            size = element.outerWidth();
+            if (o.position === "left") {
+                element.css({
+                    left: -size
+                }).show().animate({
+                    left: 0
+                }, o.duration);
+            } else {
+                element.css({
+                    right: -size
+                }).show().animate({
+                    right: 0
+                }, o.duration);
+            }
+        } else {
+            size = element.outerHeight();
+            if (o.position === "top") {
+                element.css({
+                    top: -size
+                }).show().animate({
+                    top: 0
+                }, o.duration);
+            } else {
+                element.css({
+                    bottom: -size
+                }).show().animate({
+                    bottom: 0
+                }, o.duration);
+            }
+        }
+
+        if (o.timeout > 0) {
+            this._timeout_interval = setInterval(function(){
+                if (!element.is(":hover")) {
+                    that.close();
+                    clearInterval(this._timeout_interval);
+                }
+            }, o.timeout);
+        }
+    },
+
+    _hideCharm: function(){
+        var that = this, element = this.element, o = this.options;
+        var size;
+
+        if (o.position === "left" || o.position === "right") {
+            size = element.outerWidth();
+            if (o.position === "left") {
+                element.animate({
+                    left: -size
+                }, o.duration, function(){
+                    element.hide();
+                });
+            } else {
+                element.animate({
+                    right: -size
+                }, o.duration, function(){
+                    element.hide();
+                });
+            }
+        } else {
+            size = element.outerHeight();
+            if (o.position === "top") {
+                element.animate({
+                    top: -size
+                }, o.duration, function(){
+                    element.hide();
+                });
+            } else {
+                element.animate({
+                    bottom: -size
+                }, o.duration, function(){
+                    element.hide();
+                });
+            }
+        }
+
+        clearInterval(this._timeout_interval);
+    },
+
+    open: function(){
+        var that = this, element = this.element, o = this.options;
+
+        if (element.data("opened") === true) {
+            return;
+        }
+
+        element.data("opened", true);
+        this._showCharm();
+    },
+
+    close: function(){
+        var that = this, element = this.element, o = this.options;
+
+        if (element.data("opened") === false) {
+            return;
+        }
+
+        element.data("opened", false);
+
+        this._hideCharm();
+    },
+
+    _destroy: function () {
+    },
+
+    _setOption: function ( key, value ) {
+        this._super('_setOption', key, value);
+    }
+});
