648459aea930f64f380a8308d71e7146f1c502e5
Sergey Pimenov <sergey@pimenov.com.ua>
2016-03-09 15:18:01 +0200

    METRO_AUTO_REINIT deprecated

--- js/initiator.js
+++ js/initiator.js
@@ -1,170 +1,109 @@
 $.fn.reverse = Array.prototype.reverse;
 
-$.Metro = function(params){
-    params = $.extend({
-    }, params);
-};
-
-$.Metro.hotkeys = [];
-
-$.Metro.initWidgets = function(){
-    var widgets = $("[data-role]");
-
-    var hotkeys = $("[data-hotkey]");
-    $.each(hotkeys, function(){
-        var element = $(this);
-        var hotkey = element.data('hotkey').toLowerCase();
-
-        //if ($.Metro.hotkeys.indexOf(hotkey) > -1) {
-        //    return;
-        //}
-        if (element.data('hotKeyBonded') === true ) {
-            return;
-        }
-
-        $.Metro.hotkeys.push(hotkey);
-
-        $(document).on('keyup', null, hotkey, function(e){
-            if (element === undefined) return;
-
-            if (element[0].tagName === 'A' &&
-                element.attr('href') !== undefined &&
-                element.attr('href').trim() !== '' &&
-                element.attr('href').trim() !== '#') {
-                document.location.href = element.attr('href');
-            } else {
-                element.click();
-            }
-            return false;
+$.Metro = {
+    initWidgets: function(widgets) {
+        $.each(widgets, function () {
+            var $this = $(this), w = this;
+            var roles = $this.data('role').split(/\s*,\s*/);
+            roles.map(function (func) {
+                try {
+                    //$(w)[func]();
+                    if ($.fn[func] !== undefined && $this.data(func + '-initiated') !== true) {
+                        $.fn[func].call($this);
+                        $this.data(func + '-initiated', true);
+                    }
+                } catch (e) {
+                    if (window.METRO_DEBUG) {
+                        console.log(e.message, e.stack);
+                    }
+                }
+            });
         });
+    },
 
-        element.data('hotKeyBonded', true);
-    });
+    initHotkeys: function(hotkeys){
+        $.each(hotkeys, function(){
+            var element = $(this);
+            var hotkey = element.data('hotkey').toLowerCase();
 
-    $.each(widgets, function(){
-        var $this = $(this), w = this;
-        var roles = $this.data('role').split(/\s*,\s*/);
-        roles.map(function(func){
-            try {
-                //$(w)[func]();
-                if ($.fn[func] !== undefined && $this.data(func+'-initiated') !== true) {
-                    $.fn[func].call($this);
-                    $this.data(func+'-initiated', true);
-                }
-            } catch(e) {
-                if (window.METRO_DEBUG) {
-                    console.log(e.message, e.stack);
-                }
+            //if ($.Metro.hotkeys.indexOf(hotkey) > -1) {
+            //    return;
+            //}
+            if (element.data('hotKeyBonded') === true ) {
+                return;
             }
-        });
-    });
-};
 
-$.Metro.init = function(){
-    $.Metro.initWidgets();
+            $.Metro.hotkeys.push(hotkey);
 
-    if (window.METRO_AUTO_REINIT) {
-        if (!window.canObserveMutation) {
-            var originalDOM = $('body').html(),
-                actualDOM;
+            $(document).on('keyup', null, hotkey, function(e){
+                if (element === undefined) return;
 
-            setInterval(function () {
-                actualDOM = $('body').html();
-
-                if (originalDOM !== actualDOM) {
-                    originalDOM = actualDOM;
-
-                    $.Metro.initWidgets();
+                if (element[0].tagName === 'A' &&
+                    element.attr('href') !== undefined &&
+                    element.attr('href').trim() !== '' &&
+                    element.attr('href').trim() !== '#') {
+                    document.location.href = element.attr('href');
+                } else {
+                    element.click();
                 }
-            }, 100);
-        } else {
-            var observer, observerOptions, observerCallback;
-            observerOptions = {
-                'childList': true,
-                'subtree': true
-            };
-            observerCallback = function(mutations){
+                return false;
+            });
 
-                //console.log(mutations);
+            element.data('hotKeyBonded', true);
+        });
+    },
 
-                mutations.map(function(record){
+    init: function(){
+        var widgets = $("[data-role]");
+        var hotkeys = $("[data-hotkey]");
 
-                    if (record.addedNodes) {
 
-                        /*jshint loopfunc: true */
-                        var obj, widgets, plugins, hotkeys;
+        $.Metro.initHotkeys(hotkeys);
+        $.Metro.initWidgets(widgets);
 
-                        for(var i = 0, l = record.addedNodes.length; i < l; i++) {
-                            obj = $(record.addedNodes[i]);
+        var observer, observerOptions, observerCallback;
 
-                            plugins = obj.find("[data-role]");
+        observerOptions = {
+            'childList': true,
+            'subtree': true
+        };
 
-                            hotkeys = obj.find("[data-hotkey]");
+        observerCallback = function(mutations){
 
-                            $.each(hotkeys, function(){
-                                var element = $(this);
-                                var hotkey = element.data('hotkey').toLowerCase();
+            //console.log(mutations);
 
-                                //if ($.Metro.hotkeys.indexOf(hotkey) > -1) {
-                                //    return;
-                                //}
+            mutations.map(function(record){
 
-                                if (element.data('hotKeyBonded') === true ) {
-                                    return;
-                                }
+                if (record.addedNodes) {
 
-                                $.Metro.hotkeys.push(hotkey);
+                    /*jshint loopfunc: true */
+                    var obj, widgets, plugins, hotkeys;
 
-                                $(document).on('keyup', null, hotkey, function () {
-                                    if (element === undefined) return;
+                    for(var i = 0, l = record.addedNodes.length; i < l; i++) {
+                        obj = $(record.addedNodes[i]);
 
-                                    if (element[0].tagName === 'A' &&
-                                        element.attr('href') !== undefined &&
-                                        element.attr('href').trim() !== '' &&
-                                        element.attr('href').trim() !== '#') {
-                                        document.location.href = element.attr('href');
-                                    } else {
-                                        element.click();
-                                    }
-                                    return false;
-                                });
+                        plugins = obj.find("[data-role]");
 
-                                element.data('hotKeyBonded', true);
-                                //console.log($.Metro.hotkeys);
-                            });
+                        hotkeys = obj.find("[data-hotkey]");
 
-                            if (obj.data('role') !== undefined) {
-                                widgets = $.merge(plugins, obj);
-                            } else {
-                                widgets = plugins;
-                            }
+                        $.Metro.initHotkeys(hotkeys);
 
-                            if (widgets.length) {
-                                $.each(widgets, function(){
-                                    var _this = $(this);
-                                    var roles = _this.data('role').split(/\s*,\s*/);
-                                    roles.map(function(func){
-                                        try {
-                                            if ($.fn[func] !== undefined && _this.data(func+'-initiated') !== true) {
-                                                $.fn[func].call(_this);
-                                                _this.data(func+'-initiated', true);
-                                            }
-                                        } catch(e) {
-                                            if (window.METRO_DEBUG) {
-                                                console.log(e.message, e.stack);
-                                            }
-                                        }
-                                    });
-                                });
-                            }
+                        if (obj.data('role') !== undefined) {
+                            widgets = $.merge(plugins, obj);
+                        } else {
+                            widgets = plugins;
+                        }
+
+                        if (widgets.length) {
+                            $.Metro.initWidgets(widgets);
                         }
                     }
-                });
-            };
+                }
+            });
+        };
 
-            //console.log($(document));
-            observer = new MutationObserver(observerCallback);
-            observer.observe(document, observerOptions);
-        }
+        //console.log($(document));
+        observer = new MutationObserver(observerCallback);
+        observer.observe(document, observerOptions);
     }
 };
