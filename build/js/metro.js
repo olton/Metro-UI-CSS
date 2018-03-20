acc6f2581cd16bcee22500d87dbc91ae7e033c93
Sergey Pimenov <sergey@pimenov.com.ua>
2018-03-20 12:59:09 +0200

    fix 4.1.1

--- build/js/metro.js
+++ build/js/metro.js
@@ -1,5 +1,5 @@
 /*!
- * Metro 4 Components Library v4.1.0 build @@build (https://metroui.org.ua)
+ * Metro 4 Components Library v4.1.1 build 626 (https://metroui.org.ua)
  * Copyright 2018 Sergey Pimenov
  * Licensed under MIT
  */
@@ -79,7 +79,7 @@ var isTouch = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (
 
 var Metro = {
 
-    version: "@@version-@@build@@status",
+    version: "4.1.1-626",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
     sheet: null,
