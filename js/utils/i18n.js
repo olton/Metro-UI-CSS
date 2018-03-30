77d41a258b61aede9691c746f4ec7c992f619832
Roberto Carlos García Luis <rgarcia@kairos.systems>
2018-03-30 15:12:25 -0600

    Adding es_MX i18n

--- js/utils/i18n.js
+++ js/utils/i18n.js
@@ -197,7 +197,42 @@ var Locales = {
             "no": "Ні",
             "random": "Випадково"
         }
-    }
+    },
+
+    'es-MX': {
+        "calendar": {
+            "months": [
+                "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre",
+                "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"
+            ],
+            "days": [
+                "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado",
+                "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa",
+                "Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"
+            ],
+            "time": {
+                "days": "DÍAS",
+                "hours": "HORAS",
+                "minutes": "MINS",
+                "seconds": "SEGS",
+                "month": "MES",
+                "day": "DÍA",
+                "year": "AÑO"
+            }
+        },
+        "buttons": {
+            "ok": "Aceptar",
+            "cancel": "Cancelar",
+            "done": "Hecho",
+            "today": "Hoy",
+            "now": "Ahora",
+            "clear": "Limpiar",
+            "help": "Ayuda",
+            "yes": "Si",
+            "no": "No",
+            "random": "Aleatorio"
+        }
+    },
 };
 
 Metro['locales'] = Locales;
