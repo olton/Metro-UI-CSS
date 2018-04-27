6a207c7cc6006b97ffd7074652e15ba5dfaca537
Sergey Pimenov <sergey@pimenov.com.ua>
2018-04-27 10:26:00 +0300

    improve index

--- docs/metro/js/metro.js
+++ docs/metro/js/metro.js
@@ -1,5 +1,5 @@
 /*!
- * Metro 4 Components Library v4.1.19 build 660 (https://metroui.org.ua)
+ * Metro 4 Components Library v4.1.19 build 661 (https://metroui.org.ua)
  * Copyright 2018 Sergey Pimenov
  * Licensed under MIT
  */
@@ -79,7 +79,7 @@ var isTouch = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (
 
 var Metro = {
 
-    version: "4.1.19-660",
+    version: "4.1.19-661",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
     sheet: null,
