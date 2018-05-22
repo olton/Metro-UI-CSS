b5a5a46e0d0b731ae16142dc4c1634fe4cb88e95
Sergey Pimenov <sergey@pimenov.com.ua>
2018-05-22 21:59:06 +0300

    add sorter plugin

--- build/js/metro.js
+++ build/js/metro.js
@@ -1,5 +1,5 @@
 /*
- * Metro 4 Components Library v4.2.7 build 683 (https://metroui.org.ua)
+ * Metro 4 Components Library v4.2.7 build @@build (https://metroui.org.ua)
  * Copyright 2018 Sergey Pimenov
  * Licensed under MIT
  */
@@ -80,7 +80,7 @@ var isTouch = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (
 
 var Metro = {
 
-    version: "4.2.7.683 ",
+    version: "@@version.@@build @@status",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
     sheet: null,
@@ -13684,6 +13684,157 @@ var Slider = {
 };
 
 Metro.plugin('slider', Slider);
+
+// Source: js/plugins/sorter.js
+var Sorter = {
+    init: function( options, elem ) {
+        this.options = $.extend( {}, this.options, options );
+        this.elem  = elem;
+        this.element = $(elem);
+        this.sorted = false;
+
+        this._setOptionsFromDOM();
+        this._create();
+
+        return this;
+    },
+
+    options: {
+        sortTarget: null,
+        sortContent: null,
+        sortDir: "asc",
+        onSortStart: Metro.noop,
+        onSortStop: Metro.noop,
+        onSortItemSwitch: Metro.noop,
+        onSorterCreate: Metro.noop
+    },
+
+    _setOptionsFromDOM: function(){
+        var element = this.element, o = this.options;
+
+        $.each(element.data(), function(key, value){
+            if (key in o) {
+                try {
+                    o[key] = JSON.parse(value);
+                } catch (e) {
+                    o[key] = value;
+                }
+            }
+        });
+    },
+
+    _create: function(){
+        var element = this.element, o = this.options;
+
+        this._createStructure();
+
+        Utils.exec(o.onSorterCreate, [element]);
+    },
+
+    _createStructure: function(){
+        var o = this.options;
+
+        if (o.sortTarget !== null) {
+            this.sort(o.sortDir);
+        }
+    },
+
+    _getItemContent: function(el){
+        var o = this.options;
+        var content = "", items;
+        var $el = $(el);
+
+        if (o.sortContent === null) {
+            content = el.textContent;
+        } else {
+            items = $el.find(o.sortContent);
+            if (items.length === 0) {
+                content = el.textContent;
+            } else {
+                $.each(items, function(){
+                    content += " " + this.textContent;
+                })
+            }
+        }
+
+        return content.toLowerCase().replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
+    },
+
+    sort: function(dir){
+        var element = this.element, o = this.options;
+        var switching = true, items, i, shouldSwitch, content_1, content_2;
+
+        if (dir === undefined) {
+            dir = o.sortDir;
+        }
+
+        this.sorted = false;
+
+        Utils.exec(o.onSortStart, [element], element[0]);
+
+        while (switching) {
+            switching = false;
+            items = element.find(o.sortTarget);
+            if (items.length === 0) {
+                break;
+            }
+
+            for(i = 0; i < items.length - 1; i++) {
+                shouldSwitch = false;
+
+                content_1 = this._getItemContent(items[i]);
+                content_2 = this._getItemContent(items[i + 1]);
+
+                if (dir === "asc" ? content_1 > content_2 : content_1 < content_2) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+            if (shouldSwitch) {
+                items[i].parentNode.insertBefore(items[i + 1], items[i]);
+                switching = true;
+                Utils.exec(o.onSortItemSwitch, [items[i + 1], items[i]], element[0]);
+            }
+        }
+
+        this.sorted = true;
+
+        Utils.exec(o.onSortStop, [element], element[0]);
+    },
+
+    changeAttribute: function(attributeName){
+        var that = this, element = this.element, o = this.options;
+
+        var changeSortDir = function() {
+            var dir = element.attr("data-sort-dir").trim();
+            if (dir === "") return;
+            o.sortDir = dir;
+            that.sort();
+        };
+
+        var changeSortContent = function(){
+            var content = element.attr("data-sort-content").trim();
+            if (content === "") return ;
+            o.sortContent = content;
+            that.sort();
+        };
+
+        switch (attributeName) {
+            case "data-sort-dir": changeSortDir(); break;
+            case "data-sort-content": changeSortContent(); break;
+        }
+    },
+
+    destroy: function(){}
+};
+
+Metro.plugin('sorter', Sorter);
+
+Metro['sorter'] = {
+    create: function(el, op){
+        return $(el).sorter(op);
+    }
+};
 
 // Source: js/plugins/stepper.js
 var Stepper = {
