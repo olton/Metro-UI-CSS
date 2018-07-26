8ea1c86d1ab077ad04ca24bdb98eca02c0f67bd9
Sergey Pimenov <sergey@pimenov.com.ua>
2018-07-26 21:25:24 +0300

    upd 4.2.15

--- docs/metro/js/metro.js
+++ docs/metro/js/metro.js
@@ -1,5 +1,5 @@
 /*
- * Metro 4 Components Library v4.2.15 build @@build (https://metroui.org.ua)
+ * Metro 4 Components Library v4.2.15 build 692 (https://metroui.org.ua)
  * Copyright 2018 Sergey Pimenov
  * Licensed under MIT
  */
@@ -80,8 +80,8 @@ var isTouch = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (
 
 var Metro = {
 
-    version: "@@version",
-    versionFull: "@@version.@@build @@status",
+    version: "4.2.15",
+    versionFull: "4.2.15.692 ",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
     sheet: null,
