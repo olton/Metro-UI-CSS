2f7eb8d15d78716e6d1910a9d1f5040e67a10075
Sergey Pimenov <sergey@pimenov.com.ua>
2018-09-08 19:31:35 +0300

    marge search to input

--- docs/metro/js/metro.js
+++ docs/metro/js/metro.js
@@ -10357,10 +10357,12 @@ var Input = {
         prepend: "",
         append: "",
         copyInlineStyles: true,
+        searchButton: false,
         clearButton: true,
         revealButton: true,
         clearButtonIcon: "<span class='default-icon-cross'></span>",
         revealButtonIcon: "<span class='default-icon-eye'></span>",
+        searchButtonIcon: "<span class='default-icon-search'></span>",
         customButtons: [],
 
         clsComponent: "",
@@ -10404,7 +10406,7 @@ var Input = {
         var parent = element.parent();
         var container = $("<div>").addClass("input " + element[0].className);
         var buttons = $("<div>").addClass("button-group");
-        var clearButton, revealButton;
+        var clearButton, revealButton, searchButton;
 
         if (Utils.isValue(o.historyPreset)) {
             $.each(Utils.strToArray(o.historyPreset, o.historyDivider), function(){
@@ -10430,13 +10432,17 @@ var Input = {
             element.val(o.defaultValue);
         }
 
-        if (o.clearButton !== false) {
+        if (o.clearButton === true) {
             clearButton = $("<button>").addClass("button input-clear-button").addClass(o.clsClearButton).attr("tabindex", -1).attr("type", "button").html(o.clearButtonIcon);
             clearButton.appendTo(buttons);
         }
-        if (element.attr('type') === 'password' && o.revealButton !== false) {
+        if (element.attr('type') === 'password' && o.revealButton === true) {
             revealButton = $("<button>").addClass("button input-reveal-button").addClass(o.clsRevealButton).attr("tabindex", -1).attr("type", "button").html(o.revealButtonIcon);
             revealButton.appendTo(buttons);
+        }
+        if (o.searchButton === true) {
+            searchButton = $("<button>").addClass("button input-search-button").addClass(o.clsSearchButton).attr("tabindex", -1).attr("type", o.searchButtonClick === 'submit' ? "submit" : "button").html(o.searchButtonIcon);
+            searchButton.appendTo(buttons);
         }
 
         if (o.prepend !== "") {
@@ -10514,6 +10520,14 @@ var Input = {
             Utils.exec(o.onRevealClick, [element.val()], element[0]);
         });
 
+        container.on(Metro.events.start, ".input-search-button", function(){
+            if (o.searchButtonClick !== 'submit') {
+                Utils.exec(o.onSearchButtonClick, [element.val(), $(this)], element[0]);
+            } else {
+                this.form.submit();
+            }
+        });
+
         container.on(Metro.events.stop, ".input-reveal-button", function(){
             element.attr('type', 'password').focus();
         });
@@ -10568,6 +10582,14 @@ var Input = {
 
     getHistory: function(){
         return this.history;
+    },
+
+    getHistoryIndex: function(){
+        return this.historyIndex;
+    },
+
+    setHistoryIndex: function(val){
+        this.historyIndex = val >= this.history.length ? this.history.length - 1 : val;
     },
 
     setHistory: function(history, append) {
@@ -14081,173 +14103,6 @@ var Ripple = {
 };
 
 Metro.plugin('ripple', Ripple);
-
-// Source: js/plugins/search.js
-var Search = {
-    init: function( options, elem ) {
-        this.options = $.extend( {}, this.options, options );
-        this.elem  = elem;
-        this.element = $(elem);
-
-        this._setOptionsFromDOM();
-        this._create();
-
-        Utils.exec(this.options.onInputCreate, [this.element]);
-
-        return this;
-    },
-    options: {
-        prepend: "",
-        clearButton: true,
-        searchButton: true,
-        searchButtonClick: "submit",
-        clearButtonIcon: "<span class='default-icon-cross'></span>",
-        searchButtonIcon: "<span class='default-icon-search'></span>",
-        customButtons: [],
-        copyInlineStyles: true,
-        size: "default",
-        clsSearch: "",
-        clsInput: "",
-        clsPrepend: "",
-        clsClearButton: "",
-        clsSearchButton: "",
-        clsCustomButton: "",
-        onSearchButtonClick: Metro.noop,
-        onSearchCreate: Metro.noop
-    },
-
-    _setOptionsFromDOM: function(){
-        var that = this, element = this.element, o = this.options;
-
-        $.each(element.data(), function(key, value){
-            if (key in o) {
-                try {
-                    o[key] = JSON.parse(value);
-                } catch (e) {
-                    o[key] = value;
-                }
-            }
-        });
-    },
-
-    _create: function(){
-        var that = this, element = this.element, o = this.options;
-        var prev = element.prev();
-        var parent = element.parent();
-        var container = $("<div>").addClass("input " + element[0].className);
-        var buttons = $("<div>").addClass("button-group");
-        var clearButton, searchButton;
-
-        if (element.attr("type") === undefined) {
-            element.attr("type", "text");
-        }
-
-        if (prev.length === 0) {
-            parent.prepend(container);
-        } else {
-            container.insertAfter(prev);
-        }
-
-        element.appendTo(container);
-        buttons.appendTo(container);
-
-        if (o.clearButton !== false) {
-            clearButton = $("<button>").addClass("button input-clear-button").addClass(o.clsClearButton).attr("tabindex", -1).attr("type", "button").html(o.clearButtonIcon);
-            clearButton.on(Metro.events.click, function(){
-                element.val("").trigger('change').trigger('keyup').focus();
-            });
-            clearButton.appendTo(buttons);
-        }
-        if (o.searchButton !== false) {
-            searchButton = $("<button>").addClass("button input-search-button").addClass(o.clsSearchButton).attr("tabindex", -1).attr("type", o.searchButtonClick === 'submit' ? "submit" : "button").html(o.searchButtonIcon);
-            searchButton.on(Metro.events.click, function(){
-                if (o.searchButtonClick !== 'submit') {
-                    Utils.exec(o.onSearchButtonClick, [element.val(), $(this)], element[0]);
-                } else {
-                    this.form.submit();
-                }
-            });
-            searchButton.appendTo(buttons);
-        }
-
-        if (o.prepend !== "") {
-            var prepend = Utils.isTag(o.prepend) ? $(o.prepend) : $("<span>"+o.prepend+"</span>");
-            prepend.addClass("prepend").addClass(o.clsPrepend).appendTo(container);
-        }
-
-        if (typeof o.customButtons === "string") {
-            o.customButtons = Utils.isObject(o.customButtons);
-        }
-
-        if (typeof o.customButtons === "object" && Utils.objectLength(o.customButtons) > 0) {
-            $.each(o.customButtons, function(){
-                var item = this;
-                var customButton = $("<button>").addClass("button input-custom-button").addClass(o.clsCustomButton).addClass(item.cls).attr("tabindex", -1).attr("type", "button").html(item.html);
-                customButton.on(Metro.events.click, function(){
-                    Utils.exec(item.onclick, [element.val(), customButton], element[0]);
-                });
-                customButton.appendTo(buttons);
-            });
-        }
-
-        if (element.attr('dir') === 'rtl' ) {
-            container.addClass("rtl").attr("dir", "rtl");
-        }
-
-        element[0].className = '';
-        if (o.copyInlineStyles === true) {
-            for (var i = 0, l = element[0].style.length; i < l; i++) {
-                container.css(element[0].style[i], element.css(element[0].style[i]));
-            }
-        }
-
-        container.addClass(o.clsSearch);
-        element.addClass(o.clsInput);
-
-        if (o.size !== "default") {
-            container.css({
-                width: o.size
-            });
-        }
-
-        element.on(Metro.events.blur, function(){container.removeClass("focused");});
-        element.on(Metro.events.focus, function(){container.addClass("focused");});
-
-        if (element.is(":disabled")) {
-            this.disable();
-        } else {
-            this.enable();
-        }
-    },
-
-    disable: function(){
-        //this.element.attr("disabled", true);
-        this.element.data("disabled", true);
-        this.element.parent().addClass("disabled");
-    },
-
-    enable: function(){
-        //this.element.attr("disabled", false);
-        this.element.data("disabled", false);
-        this.element.parent().removeClass("disabled");
-    },
-
-    toggleState: function(){
-        if (this.element.data("disabled") === false) {
-            this.disable();
-        } else {
-            this.enable();
-        }
-    },
-
-    changeAttribute: function(attributeName){
-        switch (attributeName) {
-            case 'disabled': this.toggleState(); break;
-        }
-    }
-};
-
-Metro.plugin('search', Search);
 
 // Source: js/plugins/select.js
 var Select = {
