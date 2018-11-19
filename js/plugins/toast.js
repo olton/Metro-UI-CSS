88a086cd7a14d2bd32f28f5ba509e679f0586c62
Sergey Pimenov <sergey@pimenov.com.ua>
2018-11-19 13:17:26 +0200

    upd toasts

--- js/plugins/toast.js
+++ js/plugins/toast.js
@@ -1,14 +1,43 @@
 var Toast = {
+
+    options: {
+        callback: Metro.noop,
+        timeout: METRO_TIMEOUT,
+        distance: 20,
+        showTop: false,
+        clsToast: ""
+    },
+
+    init: function(options){
+        this.options = $.extend({}, this.options, options);
+
+        return this;
+    },
+
     create: function(message, callback, timeout, cls){
+        var o = this.options;
         var toast = $("<div>").addClass("toast").html(message).appendTo($("body")).hide();
         var width = toast.outerWidth();
         var timer = null;
-        timeout = timeout || METRO_TIMEOUT;
+
+        timeout = timeout || o.timeout;
+        callback = callback || o.callback;
+        cls = cls || o.clsToast;
+
+        if (o.showTop === true) {
+            toast.addClass("show-top").css({
+                top: o.distance
+            });
+        } else {
+            toast.css({
+                bottom: o.distance
+            })
+        }
 
         toast.css({
             'left': '50%',
             'margin-left': -(width / 2)
-        }).addClass(cls).fadeIn(METRO_ANIMATION_DURATION);
+        }).addClass(o.clsToast).addClass(cls).fadeIn(METRO_ANIMATION_DURATION);
 
         timer = setTimeout(function(){
             timer = null;
@@ -20,4 +49,4 @@ var Toast = {
     }
 };
 
-Metro['toast'] = Toast;
+Metro['toast'] = Toast.init();
