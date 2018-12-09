60012864e5abd79fbdf5b1126f287175619c263a
Sergey Pimenov <sergey@pimenov.com.ua>
2018-12-09 13:46:21 +0200

    calendar: upd event onDayDraw

--- build/js/metro.js
+++ build/js/metro.js
@@ -100,8 +100,8 @@ var isTouch = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (
 
 var Metro = {
 
-    version: "4.2.31-dev",
-    versionFull: "4.2.31-dev",
+    version: "4.2.31-dev-9-111-2018",
+    versionFull: "4.2.31-dev-9-111-2018",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
     sheet: null,
