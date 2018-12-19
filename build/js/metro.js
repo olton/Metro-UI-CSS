8edf0c4d7f4f0cf3bd4c7326654cc02d3989edc2
Sergey Pimenov <sergey@pimenov.com.ua>
2018-12-19 16:31:08 +0200

    input file: fix on-select for FF when drop files

--- build/js/metro.js
+++ build/js/metro.js
@@ -100,8 +100,8 @@ var isTouch = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (
 
 var Metro = {
 
-    version: "@@version",
-    versionFull: "@@version.@@build @@status",
+    version: "4.2.32-dev [16:31 19-11-2018]",
+    versionFull: "4.2.32-dev [16:31 19-11-2018]",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
     sheet: null,
