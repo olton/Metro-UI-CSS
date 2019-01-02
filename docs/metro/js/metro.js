6402829f695814a3d552dd912fcc9d76b98bd640
Sergey Pimenov <sergey@pimenov.com.ua>
2019-01-02 14:12:57 +0200

    fix shift content on sidebar open and close

--- docs/metro/js/metro.js
+++ docs/metro/js/metro.js
@@ -100,8 +100,8 @@ var isTouch = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (
 
 var Metro = {
 
-    version: "@@version",
-    versionFull: "@@version.@@build @@status",
+    version: "4.2.33-dev [14:12 2-0-2019]",
+    versionFull: "4.2.33-dev [14:12 2-0-2019]",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
     sheet: null,
