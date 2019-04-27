3fb298f52c3b42291ffc461ee39a1c8d4ae55aa3
Sergey Pimenov <sergey@pimenov.com.ua>
2019-04-27 08:45:29 +0300

    Streamer: add property `html` for event with custom html

--- docs/metro/js/metro.js
+++ docs/metro/js/metro.js
@@ -119,7 +119,7 @@ var isTouch = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (
 var Metro = {
 
     version: "4.2.41",
-    compileTime: "27/04/2019 08:35:28",
+    compileTime: "27/04/2019 08:45:18",
     buildNumber: "722",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
