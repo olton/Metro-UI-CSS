aa284fd5d3286940eafb8f36cd18957ca34a1ef9
Sergey Pimenov <sergey@pimenov.com.ua>
2019-06-15 21:38:34 +0300

    upd test for metro init

--- cypress/integration/init_metro.js
+++ cypress/integration/init_metro.js
@@ -5,10 +5,29 @@ describe("Test metro 4 initialization", function(){
         'm4q-about', 'm4q-population', 'm4q-constructor', 'm4q-ajax', 'm4q-animation', 'm4q-loops', 'm4q-visibility', 'm4q-effects', 'm4q-subtree',
         'm4q-attributes', 'm4q-html', 'm4q-css', 'm4q-position', 'm4q-manipulation', 'm4q-dataset', 'm4q-events', 'm4q-utils',
 
-        'containers', 'grid', 'typography', 'tables', 'forms', 'buttons', 'images', 'figures', 'lists'
+        'containers', 'grid', 'typography', 'tables', 'forms', 'buttons', 'images', 'figures', 'lists',
+
+        'checkbox', 'file', 'input', 'input-material', 'keypad', 'rating', 'radio', 'select', 'slider', 'spinner', 'switch', 'tag-input', 'textarea',
+
+        'app-bar', 'bottom-nav', 'bottom-sheet', 'menu', 'ribbon-menu', 'sidebar', 'sidenavigation',
+
+        'accordion', 'badge', 'carousel', 'cards', 'cube', 'counter', 'charms', 'chat', 'donut', 'image-compare', 'image-magnifier', 'gravatar',
+        'list', 'listview', 'master', 'navview', 'panels', 'progress', 'streamer', 'stepper', 'splitter', 'tabs', 'tabs-material', 'table', 'tiles', 'treeview', 'wizard',
+
+        'dialog', 'info-box', 'hints', 'notify', 'popovers', 'toast', 'windows',
+
+        'calendar', 'calendarpicker', 'datepicker', 'timepicker', 'countdown', 'dateformat',
+
+        'video', 'audio',
+
+        'collapse', 'color-module', 'draggable', 'dropdown', 'validator', 'hotkeys', 'micro-tpl', 'ripple', 'storage', 'session-storage', 'sorter', 'touch',
+
+        'animation', 'border', 'clear', 'colors', 'cursors', 'display', 'embed', 'extensions', 'flex', 'functions', 'float', 'position', 'sizing', 'spacing',
+
+        'icons', 'color-schemes'
     ]
 
-    targets.forEach(function(target){
+    targets.forEach((target) => {
         it('Target - ' + target, function(){
             cy.visit(target === 'index' ? '/' : '/' + target + '.html');
         })
