3e5cd69655c368114ef1e98d34ffbd29a8b143a4
Sergey Pimenov <sergey@pimenov.com.ua>
2019-07-18 18:02:39 +0300

    upd 4.2.46

--- build/js/metro.js
+++ build/js/metro.js
@@ -119,7 +119,7 @@ var isTouch = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (
 var Metro = {
 
     version: "4.2.46",
-    compileTime: "18/07/2019 18:00:30",
+    compileTime: "18/07/2019 18:02:25",
     buildNumber: "727",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
