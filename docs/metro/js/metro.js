182e687202eb3bf9f38f855791c801ef757a948f
Sergey Pimenov <sergey@pimenov.com.ua>
2019-09-22 13:37:03 +0300

    Hint: fix remove on leave element

--- docs/metro/js/metro.js
+++ docs/metro/js/metro.js
@@ -3596,7 +3596,7 @@ var isTouch = (('ontouchstart' in window) || (navigator["MaxTouchPoints"] > 0) |
 var Metro = {
 
     version: "4.3.0",
-    compileTime: "22/09/2019 13:34:58",
+    compileTime: "22/09/2019 13:36:59",
     buildNumber: "735",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
