fba03a3e14dec8f50cb99d306bdd33f744ada9c4
Sergey Pimenov <sergey@pimenov.com.ua>
2019-11-19 18:17:11 +0200

    M4Q Events: fixed `removeEventListener` for ie11 issue #1464, Slider: fix hint for IE11

--- build/js/metro.js
+++ build/js/metro.js
@@ -1,7 +1,7 @@
 /*
  * Metro 4 Components Library v4.3.4  (https://metroui.org.ua)
  * Copyright 2012-2019 Sergey Pimenov
- * Built at 19/11/2019 16:49:41
+ * Built at 19/11/2019 18:16:59
  * Licensed under MIT
  */
 
@@ -3792,7 +3792,7 @@ var normalizeComponentName = function(name){
 var Metro = {
 
     version: "4.3.4",
-    compileTime: "19/11/2019 16:49:49",
+    compileTime: "19/11/2019 18:17:06",
     buildNumber: "742",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
