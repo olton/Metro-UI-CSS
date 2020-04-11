1a920ef9de1b5b384b2748513b64e7dd1163182a
Sergey Pimenov <sergey@pimenov.com.ua>
2020-04-11 13:03:00 +0300

    UtilsJS: improve `isFloat` function, issue #1518

--- test/metro/js/metro.js
+++ test/metro/js/metro.js
@@ -1,7 +1,7 @@
 /*
  * Metro 4 Components Library v4.3.7  (https://metroui.org.ua)
  * Copyright 2012-2020 Sergey Pimenov
- * Built at 11/04/2020 12:59:10
+ * Built at 11/04/2020 13:02:50
  * Licensed under MIT
  */
 
@@ -3780,7 +3780,7 @@ var normalizeComponentName = function(name){
 var Metro = {
 
     version: "4.3.7",
-    compileTime: "11/04/2020 12:59:17",
+    compileTime: "11/04/2020 13:02:57",
     buildNumber: "745",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
