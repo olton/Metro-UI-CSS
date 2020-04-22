32fd777f0f6d57a815e4815d216a61529ae29599
Sergey Pimenov <sergey@pimenov.com.ua>
2020-04-22 13:05:24 +0300

    ...

--- source/components/vegas/vegas.js
+++ source/components/vegas/vegas.js
@@ -1,9 +1,28 @@
+var Plugin  = {
+    _super: function(options, defaults){
+        this.options = $.extend( {}, defaults, options );
+        this._setOptionsFromDOM();
+    },
+
+    _setOptionsFromDOM: function(){
+        var element = this.element, o = this.options;
+
+        $.each(element.data(), function(key, value){
+            if (key in o) {
+                try {
+                    o[key] = JSON.parse(value);
+                } catch (e) {
+                    o[key] = value;
+                }
+            }
+        });
+    }
+}
+
 var VegasDefaultConfig = {
     duration: 4000,
     effect: "fade",
-    images: null,
-    videos: null,
-    slides: null,
+    slides: [],
     shuffle: false,
     align: "center",
     valign: "center",
@@ -23,13 +42,13 @@ if (typeof window["metroVegasSetup"] !== undefined) {
     Metro.vegasSetup(window["metroVegasSetup"]);
 }
 
-var Vegas = {
+var Vegas = $.extend({}, Plugin, {
     init: function( options, elem ) {
-        var that = this;
-
-        this.options = $.extend( {}, VegasDefaultConfig, options );
         this.elem  = elem;
         this.element = $(elem);
+
+        this._super(options, VegasDefaultConfig);
+
         this.effects = [
             "fade", "slideLeft", "slideRight",
             "slideUp", "slideDown", "zoomIn", "zoomOut",
@@ -37,38 +56,19 @@ var Vegas = {
             "kenburns", "kenburnsUp", "kenburnsDown", "kenburnsRight",
             "kenburnsLeft", "kernburnsUpLeft", "kernburnsUpRight", "kernburnsDownLeft", "kernburnsDownRight"
         ];
+
         this.support = {
             objectFit:  'objectFit'  in document.body.style,
             video: !/(Android|webOS|Phone|iPad|iPod|BlackBerry|Windows Phone)/i.test(navigator.userAgent)
         }
 
-        this.slides = [];
         this.slide = 0;
         this.current = 0;
-
-        this._setOptionsFromDOM();
-
-        ['images', 'videos'].forEach(function(el){
-            that[el] = $.isArrayLike(that.options.images) ? that.options.images : Utils.strToArray(that.options.images, ",");
-        })
+        this.slides = Utils.isObject(this.options.slides) || [];
 
         this._create();
 
         return this;
-    },
-
-    _setOptionsFromDOM: function(){
-        var that = this, element = this.element, o = this.options;
-
-        $.each(element.data(), function(key, value){
-            if (key in o) {
-                try {
-                    o[key] = JSON.parse(value);
-                } catch (e) {
-                    o[key] = value;
-                }
-            }
-        });
     },
 
     _create: function(){
@@ -84,57 +84,85 @@ var Vegas = {
 
     _createStructure: function(){
         var that = this, element = this.element, o = this.options;
-        var slide;
+        var effect = 'vegas-effect-' + o.effect, effectIn = effect + 'In', effectOut = effect + 'out';
+        var applyToInner = effect.toLowerCase().indexOf('kenburns') > -1;
 
         element.addClass("vegas");
 
-        if (this.images.length || this.videos.length) {
+        $.each(this.slides, function(){
+            var src = this;
+            var slide, inner;
 
-            slide = $("<div>").addClass("vegas__slide").append
+            slide = $("<div>").addClass("vegas__slide").appendTo(element);
 
-        }
+            if (src.image) {
+                inner = $("<div>").addClass("vegas__slide-inner").appendTo(slide);
+                inner.css({
+                    backgroundImage: 'url("'+this+'")',
+                    backgroundPosition: o.align + " " + o.valign,
+                    backgroundSize: o.cover === true ? 'cover' : 'auto',
+                    backgroundRepeat: o.cover !== true ? 'repeat' : 'auto'
+                });
+            } else {
 
-        if (o.video === true) {
-            var video = $("<video>").addClass("vegas__video").appendTo(element);
-            $.each(this.src, function(){
-                $("<source>").attr("src", this).appendTo(video);
-            })
-            if (this.support.objectFit) {
-                video.css({
-                    objectPosition: o.align + " " + o.valign,
-                    objectFit: 'cover',
-                    width: '100%',
-                    height: '100%'
-                })
-            } else if (o.cover === true) {
-                video.css({
-                    width: '100%',
-                    height: '100%'
-                })
             }
-            video[0].loop = o.loop;
-            video[0].autoplay = o.autoplay;
-            video[0].mute = o.mute;
-        } else {
-            $.each(this.src, function(){
-                var slide = $("<div>").addClass("vegas__slide").appendTo(element);
-                var inner = $("<div>").addClass("vegas__slide-inner")
-                    .css({
-                        backgroundImage: 'url("'+this+'")',
-                        backgroundPosition: o.align + " " + o.valign,
-                        backgroundSize: o.cover === true ? 'cover' : 'auto',
-                        backgroundRepeat: o.cover !== true ? 'repeat' : 'auto'
-                    }).appendTo(slide);
 
-                that.slides.push(slide[0]);
-            })
-        }
+            if (applyToInner) {
+                inner.addClass(effect)
+            } else {
+                slide.addClass(effect)
+            }
+        })
 
-        if (o.preload) {
-            this._preload();
-        }
+        // if (this.images.length || this.videos.length) {
+        //
+        //     slide = $("<div>").addClass("vegas__slide").append
+        //
+        // }
+        //
+        // if (o.video === true) {
+        //     var video = $("<video>").addClass("vegas__video").appendTo(element);
+        //     $.each(this.src, function(){
+        //         $("<source>").attr("src", this).appendTo(video);
+        //     })
+        //     if (this.support.objectFit) {
+        //         video.css({
+        //             objectPosition: o.align + " " + o.valign,
+        //             objectFit: 'cover',
+        //             width: '100%',
+        //             height: '100%'
+        //         })
+        //     } else if (o.cover === true) {
+        //         video.css({
+        //             width: '100%',
+        //             height: '100%'
+        //         })
+        //     }
+        //     video[0].loop = o.loop;
+        //     video[0].autoplay = o.autoplay;
+        //     video[0].mute = o.mute;
+        // } else {
+        //     $.each(this.src, function(){
+        //         var slide = $("<div>").addClass("vegas__slide").appendTo(element);
+        //         var inner = $("<div>").addClass("vegas__slide-inner")
+        //             .css({
+        //                 backgroundImage: 'url("'+this+'")',
+        //                 backgroundPosition: o.align + " " + o.valign,
+        //                 backgroundSize: o.cover === true ? 'cover' : 'auto',
+        //                 backgroundRepeat: o.cover !== true ? 'repeat' : 'auto'
+        //             }).appendTo(slide);
+        //
+        //         that.slides.push(slide[0]);
+        //     })
+        // }
+        //
+        // if (o.preload) {
+        //     this._preload();
+        // }
+        //
+        // that.play();
 
-        that.play();
+        this._go(0);
     },
 
     _createEvents: function(){
@@ -162,15 +190,9 @@ var Vegas = {
 
     },
 
-    _go: function(){
+    _go: function(n){
         var that = this, element = this.element, o = this.options;
-        var effect = 'vegas-effect-' + o.effect, effectIn = effect + 'In', effectOut = effect + 'out';
 
-        var current = this.slides[this.current];
-        var next = this.slides[this.slide];
-
-        current.css('transition-duration', o.duration + 'ms').addClass(effectOut);
-        next.css('transition-duration', o.duration + 'ms').addClass(effectIn);
     },
 
     _end: function(){
@@ -183,14 +205,14 @@ var Vegas = {
         var that = this, element = this.element, o = this.options;
         var effect = 'vegas-effect-' + o.effect, effectIn = effect + '-in', effectOut = effect + '-out';
 
-        $.each(this.slides, function(){
-            var slide = $(this);
-            slide.addClass(effect);
-        })
-
-        $(this.slides[this.current])
-            .css('transition-duration', o.duration + 'ms')
-            .addClass(effectIn);
+        // $.each(this.slides, function(){
+        //     var slide = $(this);
+        //     slide.addClass(effect);
+        // })
+        //
+        // $(this.slides[this.current])
+        //     .css('transition-duration', o.duration + 'ms')
+        //     .addClass(effectIn);
     },
 
     pause: function(){
@@ -252,6 +274,6 @@ var Vegas = {
     },
 
     destroy: function(){}
-};
+});
 
 Metro.plugin('vegas', Vegas);
