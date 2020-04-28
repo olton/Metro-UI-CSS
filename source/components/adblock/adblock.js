8494a68c79c898ceb7cbdee56ce34230de15cb26
Sergey Pimenov <sergey@pimenov.com.ua>
2020-04-28 23:46:37 +0300

    add adblock

--- source/components/adblock/adblock.js
+++ source/components/adblock/adblock.js
@@ -15,7 +15,7 @@ if (typeof window["metroAdblockSetup"] !== undefined) {
 
 var Adblock = {
     bite: function(){
-        var classes = "adsense google-adsense dblclick advert topad top_ads topAds textads sponsoredtextlink_container show_ads right-banner rekl mpu module-ad mid_ad mediaget horizontal_ad headerAd contentAd brand-link bottombanner bottom_ad_block block_ad bannertop banner-right banner-body b-banner b-article-aside__banner b-advert adwrapper adverts advertisment advertisement:not(body) advertise advert_list adtable adsense adpic adlist adleft adinfo adi adholder adframe addiv ad_text ad_space ad_right ad_links ad_body ad_block ad_Right adTitle adText";
+        var classes = "adblock-bite adsense google-adsense dblclick advert topad top_ads topAds textads sponsoredtextlink_container show_ads right-banner rekl mpu module-ad mid_ad mediaget horizontal_ad headerAd contentAd brand-link bottombanner bottom_ad_block block_ad bannertop banner-right banner-body b-banner b-article-aside__banner b-advert adwrapper adverts advertisment advertisement:not(body) advertise advert_list adtable adsense adpic adlist adleft adinfo adi adholder adframe addiv ad_text ad_space ad_right ad_links ad_body ad_block ad_Right adTitle adText";
         $("<div>")
             .addClass(classes.split(" ").shuffle().join(" "))
             .css({
@@ -36,7 +36,7 @@ var Adblock = {
         var checkStop = Adblock.options.checkStop;
         var interval = false;
         var run = function(){
-            var a = $(".adsense.google-adsense.dblclick.advert");
+            var a = $(".adsense.google-adsense.dblclick.advert.adblock-bite");
             var b = a.find("a");
             var done = function(){
                 clearInterval(interval);
@@ -75,6 +75,8 @@ var Adblock = {
         run();
     }
 }
+
+Metro['Adblock'] = Adblock;
 
 $(function(){
     Adblock.options = $.extend({}, AdblockDefaultConfig);
--- test/metro/js/metro.js
+++ test/metro/js/metro.js
@@ -1,7 +1,7 @@
 /*
  * Metro 4 Components Library v4.3.7  (https://metroui.org.ua)
  * Copyright 2012-2020 Sergey Pimenov
- * Built at 28/04/2020 16:31:27
+ * Built at 28/04/2020 23:44:49
  * Licensed under MIT
  */
 
@@ -4307,7 +4307,7 @@ var normalizeComponentName = function(name){
 var Metro = {
 
     version: "4.3.7",
-    compileTime: "28/04/2020 16:31:35",
+    compileTime: "28/04/2020 23:44:56",
     buildNumber: "745",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
@@ -8323,7 +8323,7 @@ if (typeof window["metroAdblockSetup"] !== undefined) {
 
 var Adblock = {
     bite: function(){
-        var classes = "adsense google-adsense dblclick advert topad top_ads topAds textads sponsoredtextlink_container show_ads right-banner rekl mpu module-ad mid_ad mediaget horizontal_ad headerAd contentAd brand-link bottombanner bottom_ad_block block_ad bannertop banner-right banner-body b-banner b-article-aside__banner b-advert adwrapper adverts advertisment advertisement:not(body) advertise advert_list adtable adsense adpic adlist adleft adinfo adi adholder adframe addiv ad_text ad_space ad_right ad_links ad_body ad_block ad_Right adTitle adText";
+        var classes = "adblock-bite adsense google-adsense dblclick advert topad top_ads topAds textads sponsoredtextlink_container show_ads right-banner rekl mpu module-ad mid_ad mediaget horizontal_ad headerAd contentAd brand-link bottombanner bottom_ad_block block_ad bannertop banner-right banner-body b-banner b-article-aside__banner b-advert adwrapper adverts advertisment advertisement:not(body) advertise advert_list adtable adsense adpic adlist adleft adinfo adi adholder adframe addiv ad_text ad_space ad_right ad_links ad_body ad_block ad_Right adTitle adText";
         $("<div>")
             .addClass(classes.split(" ").shuffle().join(" "))
             .css({
@@ -8344,7 +8344,7 @@ var Adblock = {
         var checkStop = Adblock.options.checkStop;
         var interval = false;
         var run = function(){
-            var a = $(".adsense.google-adsense.dblclick.advert");
+            var a = $(".adsense.google-adsense.dblclick.advert.adblock-bite");
             var b = a.find("a");
             var done = function(){
                 clearInterval(interval);
@@ -8383,6 +8383,8 @@ var Adblock = {
         run();
     }
 }
+
+Metro['Adblock'] = Adblock;
 
 $(function(){
     Adblock.options = $.extend({}, AdblockDefaultConfig);
