7af4ae074e84cdea0a050a1c53deedc1e920406b
Sergey Pimenov <sergey@pimenov.com.ua>
2020-05-03 06:32:49 +0300

    structure refactoring

--- source/i18n/de-DE.js
+++ source/i18n/de-DE.js
@@ -1,39 +1,35 @@
-(function(){
-    var l = {
-        'de-DE': {
-            "calendar": {
-                "months": [
-                    "Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember",
-                    "Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"
-                ],
-                "days": [
-                    "Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag",
-                    "So", "Mo", "Di", "Mi", "Do", "Fr", "Sa",
-                    "Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam"
-                ],
-                "time": {
-                    "days": "TAGE",
-                    "hours": "STD",
-                    "minutes": "MIN",
-                    "seconds": "SEK"
-                }
-            },
-            "buttons": {
-                "ok": "OK",
-                "cancel": "Abbrechen",
-                "done": "Fertig",
-                "today": "Heute",
-                "now": "Jetzt",
-                "clear": "Löschen",
-                "help": "Hilfe",
-                "yes": "Ja",
-                "no": "Nein",
-                "random": "Zufällig",
-                "save": "Speichern",
-                "reset": "Zurücksetzen"
+$.extend(Metro['locales'], {
+    'de-DE': {
+        "calendar": {
+            "months": [
+                "Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember",
+                "Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"
+            ],
+            "days": [
+                "Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag",
+                "So", "Mo", "Di", "Mi", "Do", "Fr", "Sa",
+                "Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam"
+            ],
+            "time": {
+                "days": "TAGE",
+                "hours": "STD",
+                "minutes": "MIN",
+                "seconds": "SEK"
             }
+        },
+        "buttons": {
+            "ok": "OK",
+            "cancel": "Abbrechen",
+            "done": "Fertig",
+            "today": "Heute",
+            "now": "Jetzt",
+            "clear": "Löschen",
+            "help": "Hilfe",
+            "yes": "Ja",
+            "no": "Nein",
+            "random": "Zufällig",
+            "save": "Speichern",
+            "reset": "Zurücksetzen"
         }
     }
-
-    $.extend(Metro['locales'], l);
-})()
+});
