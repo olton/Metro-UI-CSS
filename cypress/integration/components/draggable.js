71ec35015c523d6e98c416004d51c6ea46a5b553
Sergey Pimenov <sergey@pimenov.com.ua>
2020-06-01 14:37:23 +0300

    fix components for component constructor

---
+++ cypress/integration/components/draggable.js
@@ -0,0 +1,7 @@
+const name = 'draggable';
+
+describe("Metro 4 :: Draggable", () => {
+    it('Component Initialization', ()=>{
+        cy.visit("cypress/"+name+".html");
+    })
+})
---
+++ cypress/integration/components/radio.js
@@ -0,0 +1,7 @@
+const name = 'radio';
+
+describe("Metro 4 :: Radio", () => {
+    it('Component Initialization', ()=>{
+        cy.visit("cypress/"+name+".html");
+    })
+})
