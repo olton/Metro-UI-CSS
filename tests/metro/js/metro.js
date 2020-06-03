7dc37009e3cb64f0f4af5bd744cd9bcfa2f5ef9d
Sergey Pimenov <sergey@pimenov.com.ua>
2020-06-03 15:42:27 +0300

    fix components for component constructor

--- tests/metro/js/metro.js
+++ tests/metro/js/metro.js
@@ -1,7 +1,7 @@
 /*
  * Metro 4 Components Library v4.3.8  (https://metroui.org.ua)
  * Copyright 2012-2020 Sergey Pimenov
- * Built at 03/06/2020 14:17:26
+ * Built at 03/06/2020 15:40:55
  * Licensed under MIT
  */
 
@@ -4364,7 +4364,7 @@ var normalizeComponentName = function(name){
 var Metro = {
 
     version: "4.3.8",
-    compileTime: "03/06/2020 14:17:28",
+    compileTime: "03/06/2020 15:40:57",
     buildNumber: "746",
     isTouchable: isTouch,
     fullScreenEnabled: document.fullscreenEnabled,
@@ -28024,209 +28024,221 @@ $.extend(Metro['locales'], {
     });
 }(Metro, m4q));
 
-var MaterialTabsDefaultConfig = {
-    materialtabsDeferred: 0,
-    deep: false,
-    fixedTabs: false,
+(function(Metro, $) {
+    var Utils = Metro.utils;
+    var MaterialTabsDefaultConfig = {
+        materialtabsDeferred: 0,
+        deep: false,
+        fixedTabs: false,
+        duration: 300,
+        appBar: false,
 
-    clsComponent: "",
-    clsTab: "",
-    clsTabActive: "",
-    clsMarker: "",
+        clsComponent: "",
+        clsTabs: "",
+        clsTab: "",
+        clsTabActive: "",
+        clsMarker: "",
 
-    onBeforeTabOpen: Metro.noop_true,
-    onTabOpen: Metro.noop,
-    onTabsScroll: Metro.noop,
-    onTabsCreate: Metro.noop
-};
+        onBeforeTabOpen: Metro.noop_true,
+        onTabOpen: Metro.noop,
+        onTabsScroll: Metro.noop,
+        onTabsCreate: Metro.noop
+    };
 
-Metro.materialTabsSetup = function (options) {
-    MaterialTabsDefaultConfig = $.extend({}, MaterialTabsDefaultConfig, options);
-};
+    Metro.materialTabsSetup = function (options) {
+        MaterialTabsDefaultConfig = $.extend({}, MaterialTabsDefaultConfig, options);
+    };
 
-if (typeof window["metroMaterialTabsSetup"] !== undefined) {
-    Metro.materialTabsSetup(window["metroMaterialTabsSetup"]);
-}
+    if (typeof window["metroMaterialTabsSetup"] !== undefined) {
+        Metro.materialTabsSetup(window["metroMaterialTabsSetup"]);
+    }
 
-Component('material-tabs', {
-    init: function( options, elem ) {
-        this._super(elem, options, MaterialTabsDefaultConfig);
-
-        this.marker = null;
-        this.scroll = 0;
-        this.scrollDir = "left";
-
-        Metro.createExec(this);
-
-        return this;
-    },
-
-    _create: function(){
-        var element = this.element, o = this.options;
-
-        Metro.checkRuntime(element, this.name);
-
-        this._createStructure();
-        this._createEvents();
-
-        Utils.exec(o.onTabsCreate, [element], element[0]);
-        element.fire("tabscreate");
-    },
-
-    _applyColor: function(to, color, option){
-
-        to = $(to);
-
-        if (Utils.isValue(color)) {
-            if (Utils.isColor(color)) {
-                to.css(option, color);
-            } else {
-                to.addClass(color);
-            }
-        }
-    },
-
-    _createStructure: function(){
-        var element = this.element, o = this.options;
-        var tabs = element.find("li"), active_tab = element.find("li.active");
-
-        element.addClass("tabs-material").addClass(o.clsComponent);
-        tabs.addClass(o.clsTab);
-
-        if (o.deep === true) {
-            element.addClass("deep");
-        }
-
-        if (o.fixedTabs === true) {
-            element.addClass("fixed-tabs");
-        }
-
-        this.marker = element.find(".tab-marker");
-
-        if (this.marker.length === 0) {
-            this.marker = $("<span>").addClass("tab-marker").addClass(o.clsMarker).appendTo(element);
-        }
-
-        this.openTab(active_tab.length === 0 ? tabs[0] : active_tab[0]);
-    },
-
-    _createEvents: function(){
-        var that = this, element = this.element, o = this.options;
-
-        element.on(Metro.events.click, "li", function(e){
-            var tab = $(this);
-            var active_tab = element.find("li.active");
-            var tab_next = tab.index() > active_tab.index();
-            var target = tab.children("a").attr("href");
-
-            if (Utils.isValue(target) && target[0] === "#") {
-                if (tab.hasClass("active")) return;
-                if (tab.hasClass("disabled")) return;
-                if (Utils.exec(o.onBeforeTabOpen, [tab, target, tab_next], this) === false) return;
-                that.openTab(tab, tab_next);
-                e.preventDefault();
-            }
-        });
-
-        element.on(Metro.events.scroll, function(){
-            var oldScroll = this.scroll;
-
-            this.scrollDir = this.scroll < element[0].scrollLeft ? "left" : "right";
-            this.scroll = element[0].scrollLeft;
-
-            Utils.exec(o.onTabsScroll, [element[0].scrollLeft, oldScroll, this.scrollDir], element[0]);
-
-            element.fire("tabsscroll", {
-                scrollLeft: element[0].scrollLeft,
-                oldScroll: oldScroll,
-                scrollDir: that.scrollDir
+    Metro.Component('material-tabs', {
+        init: function( options, elem ) {
+            this._super(elem, options, MaterialTabsDefaultConfig, {
+                marker: null,
+                scroll: 0,
+                scrollDir: "left"
             });
 
-        });
-    },
+            return this;
+        },
 
-    openTab: function(tab, tab_next){
-        var element = this.element, o = this.options;
-        var tabs = element.find("li");
-        var magic = 52, shift, width, tab_width, target, tab_left, scroll, scrollLeft;
+        _create: function(){
+            var element = this.element;
 
-        tab = $(tab);
+            this._createStructure();
+            this._createEvents();
 
-        $.each(tabs, function(){
-            var target = $(this).find("a").attr("href");
-            if (!Utils.isValue(target)) return;
-            if (target[0] === "#" && target.length > 1) {
-                $(target).hide();
+            this._fireEvent("tabs-create", {
+                element: element
+            });
+        },
+
+        _applyColor: function(to, color, option){
+
+            to = $(to);
+
+            if (Utils.isValue(color)) {
+                if (Utils.isColor(color)) {
+                    to.css(option, color);
+                } else {
+                    to.addClass(color);
+                }
             }
-        });
+        },
 
-        width = element.width();
-        scroll = element.scrollLeft();
-        tab_left = tab.position().left;
-        tab_width = tab.width();
-        shift = tab_left + tab_width;
+        _createStructure: function(){
+            var element = this.element, o = this.options;
+            var tabs = element.find("li"), active_tab = element.find("li.active");
+            var wrapper = $("<div>").addClass("tabs-material-wrapper").addClass(o.clsComponent).insertBefore(element);
 
-        tabs.removeClass("active").removeClass(o.clsTabActive);
-        tab.addClass("active").addClass(o.clsTabActive);
+            if (o.appBar === true) {
+                wrapper.addClass("app-bar-present");
+            }
+            if (o.appBar === "more") {
+                wrapper.addClass("app-bar-present-more");
+            }
 
-        if (shift + magic > width + scroll) {
-            scrollLeft = scroll + (magic * 2);
-        } else if (tab_left < scroll) {
-            scrollLeft = tab_left - magic * 2;
-        } else {
-            scrollLeft = scroll;
+            element.appendTo(wrapper);
+            element.addClass("tabs-material").addClass(o.clsTabs);
+            tabs.addClass(o.clsTab);
+
+            if (o.deep === true) {
+                element.addClass("deep");
+            }
+
+            if (o.fixedTabs === true) {
+                element.addClass("fixed-tabs");
+            }
+
+            this.marker = element.find(".tab-marker");
+
+            if (this.marker.length === 0) {
+                this.marker = $("<span>").addClass("tab-marker").addClass(o.clsMarker).appendTo(element);
+            }
+
+            this.openTab(active_tab.length === 0 ? tabs[0] : active_tab[0]);
+        },
+
+        _createEvents: function(){
+            var that = this, element = this.element, o = this.options;
+
+            element.on(Metro.events.click, "li", function(e){
+                var tab = $(this);
+                var active_tab = element.find("li.active");
+                var tab_next = tab.index() > active_tab.index();
+                var target = tab.children("a").attr("href");
+
+                if (Utils.isValue(target) && target[0] === "#") {
+                    if (tab.hasClass("active")) return;
+                    if (tab.hasClass("disabled")) return;
+                    if (Utils.exec(o.onBeforeTabOpen, [tab, target, tab_next], this) === false) return;
+                    that.openTab(tab, tab_next);
+                    e.preventDefault();
+                }
+            });
+
+            element.on(Metro.events.scroll, function(){
+                var oldScroll = this.scroll;
+
+                this.scrollDir = this.scroll < element[0].scrollLeft ? "left" : "right";
+                this.scroll = element[0].scrollLeft;
+
+                Utils.exec(o.onTabsScroll, [element[0].scrollLeft, oldScroll, this.scrollDir], element[0]);
+
+                element.fire("tabsscroll", {
+                    scrollLeft: element[0].scrollLeft,
+                    oldScroll: oldScroll,
+                    scrollDir: that.scrollDir
+                });
+
+            });
+        },
+
+        openTab: function(tab, tab_next){
+            var element = this.element, o = this.options;
+            var tabs = element.find("li");
+            var magic = 52, shift, width, tab_width, target, tab_left, scroll, scrollLeft;
+
+            tab = $(tab);
+
+            $.each(tabs, function(){
+                var target = $(this).find("a").attr("href");
+                if (!Utils.isValue(target)) return;
+                if (target[0] === "#" && target.length > 1) {
+                    $(target).hide();
+                }
+            });
+
+            width = element.width();
+            scroll = element.scrollLeft();
+            tab_left = tab.position().left;
+            tab_width = tab.width();
+            shift = tab_left + tab_width;
+
+            tabs.removeClass("active").removeClass(o.clsTabActive);
+            tab.addClass("active").addClass(o.clsTabActive);
+
+            if (shift + magic > width + scroll) {
+                scrollLeft = scroll + (magic * 2);
+            } else if (tab_left < scroll) {
+                scrollLeft = tab_left - magic * 2;
+            } else {
+                scrollLeft = scroll;
+            }
+
+            element.animate({
+                draw: {
+                    scrollLeft: scrollLeft
+                },
+                dur: o.duration
+            });
+
+            this.marker.animate({
+                draw: {
+                    left: tab_left,
+                    width: tab_width
+                },
+                dur: o.duration
+            });
+
+            target = tab.find("a").attr("href");
+            if (Utils.isValue(target)) {
+                if (target[0] === "#" && target.length > 1) {
+                    $(target).show();
+                }
+            }
+
+            this._fireEvent("tab-open", {
+                tab: tab[0],
+                target: target,
+                tab_next: tab_next
+            });
+        },
+
+        open: function(tab_num){
+            var element = this.element;
+            var tabs = element.find("li");
+            var active_tab = element.find("li.active");
+            var tab = tabs.eq(tab_num - 1);
+            var tab_next = tabs.index(tab) > tabs.index(active_tab);
+            this.openTab(tab, tab_next);
+        },
+
+        changeAttribute: function(){
+        },
+
+        destroy: function(){
+            var element = this.element;
+
+            element.off(Metro.events.click, "li");
+            element.off(Metro.events.scroll);
+
+            return element;
         }
-
-        element.animate({
-            draw: {
-                scrollLeft: scrollLeft
-            }
-        });
-
-        this.marker.animate({
-            draw: {
-                left: tab_left,
-                width: tab_width
-            }
-        });
-
-        target = tab.find("a").attr("href");
-        if (Utils.isValue(target)) {
-            if (target[0] === "#" && target.length > 1) {
-                $(target).show();
-            }
-        }
-
-        Utils.exec(o.onTabOpen, [tab[0], target, tab_next], element[0]);
-        element.fire("tabopen", {
-            tab: tab[0],
-            target: target,
-            tab_next: tab_next
-        });
-    },
-
-    open: function(tab_num){
-        var element = this.element;
-        var tabs = element.find("li");
-        var active_tab = element.find("li.active");
-        var tab = tabs.eq(tab_num - 1);
-        var tab_next = tabs.index(tab) > tabs.index(active_tab);
-        this.openTab(tab, tab_next);
-    },
-
-    changeAttribute: function(){
-    },
-
-    destroy: function(){
-        var element = this.element;
-
-        element.off(Metro.events.click, "li");
-        element.off(Metro.events.scroll);
-
-        return element;
-    }
-});
-
+    });
+}(Metro, m4q));
 
 (function(Metro, $) {
     var Utils = Metro.utils;
