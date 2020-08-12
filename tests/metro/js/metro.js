fef6fb816c74852fae89efcde72a604fe0ced431
Sergey Pimenov <sergey@pimenov.com.ua>
2020-08-12 21:52:17 +0300

    Tokenizer: new component to create tokenized text

--- tests/metro/js/metro.js
+++ tests/metro/js/metro.js
@@ -1,7 +1,7 @@
 /*
  * Metro 4 Components Library v4.4.0  (https://metroui.org.ua)
  * Copyright 2012-2020 Sergey Pimenov
- * Built at 10/08/2020 21:27:23
+ * Built at 12/08/2020 21:48:41
  * Licensed under MIT
  */
 (function (global, undefined) {
@@ -4507,7 +4507,7 @@ $.noConflict = function() {
     var Metro = {
 
         version: "4.4.0",
-        compileTime: "10/08/2020 21:27:23",
+        compileTime: "12/08/2020 21:48:41",
         buildNumber: "750",
         isTouchable: isTouch,
         fullScreenEnabled: document.fullscreenEnabled,
@@ -30960,6 +30960,101 @@ $.noConflict = function() {
 
     Metro['toast'] = Toast;
     Metro['createToast'] = Toast.create;
+}(Metro, m4q));
+
+/* eslint-disable */
+(function(Metro, $) {
+    'use strict';
+
+    var TokenizerDefaultConfig = {
+        spaceSymbol: "",
+        spaceClass: "",
+        tokenClass: "",
+        splitter: "",
+        tokenElement: "span",
+        clsTokenizer: "",
+        clsToken: "",
+        clsTokenOdd: "",
+        clsTokenEven: "",
+        onTokenizerCreate: Metro.noop
+    };
+
+    Metro.tokenizerSetup = function (options) {
+        TokenizerDefaultConfig = $.extend({}, TokenizerDefaultConfig, options);
+    };
+
+    if (typeof window["metroTokenizerSetup"] !== undefined) {
+        Metro.tokenizerSetup(window["metroTokenizerSetup"]);
+    }
+
+    Metro.Component('tokenizer', {
+        init: function( options, elem ) {
+            this._super(elem, options, TokenizerDefaultConfig, {
+                // define instance vars here
+            });
+            return this;
+        },
+
+        _create: function(){
+            var that = this, element = this.element, o = this.options;
+
+            this._createStructure();
+            this._createEvents();
+
+            this._fireEvent('tokenizer-create');
+        },
+
+        _createStructure: function(){
+            var that = this, element = this.element, o = this.options;
+            var result = "", text, index = 1;
+
+            text = element.text().trim().replace(/[\r\n\t]/gi, '').replace(/\s\s+/g, " ");
+
+            $.each(text.split(o.splitter), function(i){
+                var symbol = this;
+                var isSpace = symbol === " ";
+                var token;
+
+                token = $("<"+o.tokenElement+">")
+                    .html(isSpace ? o.spaceSymbol : symbol)
+                    .attr("aria-hidden", true)
+                    .addClass(isSpace ? o.spaceClass : "")
+                    .addClass(isSpace ? "" : "item-"+symbol.replace(" ", "_"))
+                    .addClass(o.tokenClass !== "" ? o.tokenClass + "-" + (i+1) : "")
+                    .addClass(o.clsToken);
+
+                if (!isSpace) {
+                    index++;
+                }
+
+                if (!isSpace) {
+                    token.addClass(index % 2 === 0 ? "item-even" : "item-odd");
+                }
+
+                result += token.outerHTML()+"\n";
+
+            });
+
+            element
+                .attr("aria-label", text)
+                .addClass("tokenizer")
+                .addClass(o.clsTokenizer)
+                .clear()
+                .html(result);
+        },
+
+        _createEvents: function(){
+            var that = this, element = this.element, o = this.options;
+
+        },
+
+        changeAttribute: function(){
+        },
+
+        destroy: function(){
+            this.element.remove();
+        }
+    });
 }(Metro, m4q));
 
 (function(Metro, $) {
