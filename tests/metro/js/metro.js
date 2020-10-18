ad9acb3a0665f33aede084522c2fcfb8cdb4b714
Sergey Pimenov <sergey@pimenov.com.ua>
2020-10-19 00:06:38 +0300

    button add shadowed class

--- tests/metro/js/metro.js
+++ tests/metro/js/metro.js
@@ -1,7 +1,7 @@
 /*
  * Metro 4 Components Library v4.4.1  (https://metroui.org.ua)
  * Copyright 2012-2020 Sergey Pimenov
- * Built at 18/10/2020 20:12:20
+ * Built at 19/10/2020 00:06:18
  * Licensed under GPL3
  */
 (function (global, undefined) {
@@ -4537,7 +4537,7 @@ $.noConflict = function() {
     var Metro = {
 
         version: "4.4.1",
-        compileTime: "18/10/2020 20:12:20",
+        compileTime: "19/10/2020 00:06:18",
         buildNumber: "@@build",
         isTouchable: isTouch,
         fullScreenEnabled: document.fullscreenEnabled,
