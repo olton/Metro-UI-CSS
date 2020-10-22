4671b0228c33c941862fb58d3966df4b03905e78
Sergey Pimenov <sergey@pimenov.com.ua>
2020-10-22 13:26:26 +0300

    GradientBox: refactoring

--- source/components/gradient-box/gradient-box.js
+++ source/components/gradient-box/gradient-box.js
@@ -47,19 +47,20 @@
             this.func = (this.repeat ? "repeating-" : "") + this.type + "-gradient";
 
 
-            if (this.type === "linear" && o.gradientPosition === "") {
-                this.position = "to bottom";
-            } else {
-                this.position = o.gradientPosition.toLowerCase();
-            }
+            if (this.type === "linear") {
+                if ( !o.gradientPosition ) {
+                    this.position = "to bottom";
+                } else {
+                    this.position = isNaN(o.gradientPosition) === false ? o.gradientPosition + "deg" : o.gradientPosition;
 
-            if (this.type === "radial") {
-                if (this.position && this.position.indexOf("at ") === -1) {
-                    this.position = "at " + this.position;
+                    if (this.position.indexOf("deg") === -1 && this.position.indexOf("to ") === -1) {
+                        this.position = "to " + this.position;
+                    }
                 }
             } else {
-                if (isNaN(parseInt(this.position)) && this.position.indexOf("to ") === -1) {
-                    this.position = "to " + this.position;
+                this.position = o.gradientPosition.toLowerCase();
+                if (this.position && this.position.indexOf("at ") === -1) {
+                    this.position = "at " + this.position;
                 }
             }
 
@@ -80,7 +81,7 @@
                 gradientOptions.push(this.shape);
             }
 
-            if (this.size) {
+            if (this.type === "radial" && this.size) {
                 gradientOptions.push(this.size);
             }
 
