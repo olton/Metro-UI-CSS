f07c9184b805da239b017e2d68d854eae08f62a2
Sergey Pimenov <sergey@pimenov.com.ua>
2020-10-31 15:00:45 +0200

    Default icons: optimized styles, Media players: optimized default icons

--- build/js/metro.js
+++ build/js/metro.js
@@ -1,7 +1,7 @@
 /*
  * Metro 4 Components Library v4.4.2  (https://metroui.org.ua)
  * Copyright 2012-2020 Sergey Pimenov
- * Built at 31/10/2020 14:35:34
+ * Built at 31/10/2020 15:00:20
  * Licensed under GPL3
  */
 (function (global, undefined) {
@@ -4537,7 +4537,7 @@ $.noConflict = function() {
     var Metro = {
 
         version: "4.4.2",
-        compileTime: "31/10/2020 14:35:34",
+        compileTime: "31/10/2020 15:00:20",
         buildNumber: "@@build",
         isTouchable: isTouch,
         fullScreenEnabled: document.fullscreenEnabled,
