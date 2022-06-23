732317183d1260ce5c186c5ea1ea012949d7545c
Serhii Pimenov <serhii@pimenov.com.ua>
2022-06-23 09:19:57 +0300

    release 4.5.1

--- tests/metro/js/metro.js
+++ tests/metro/js/metro.js
@@ -1,7 +1,7 @@
 /*
  * Metro 4 Components Library v4.5.1  (https://metroui.org.ua)
  * Copyright 2012-2022 Sergey Pimenov
- * Built at 27/04/2022 11:27:09
+ * Built at 23/06/2022 09:19:46
  * Licensed under MIT
  */
 /*!
@@ -7239,7 +7239,7 @@ $.noConflict = function() {
     var Metro = {
 
         version: "4.5.1",
-        compileTime: "27/04/2022 11:27:09",
+        compileTime: "23/06/2022 09:19:46",
         buildNumber: "@@build",
         isTouchable: isTouch,
         fullScreenEnabled: document.fullscreenEnabled,
