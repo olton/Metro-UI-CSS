9c301c42a0ded2efa44ddb535186c7dddc00656f
Serhii Pimenov <serhii@pimenov.com.ua>
2024-12-12 11:12:36 +0200

    road to 5.1

--- source/animation/index.js
+++ source/animation/index.js
@@ -1,3 +1,60 @@
 import {Animation} from "@olton/animation";
 
 globalThis["Animation"] = Animation
+
+if (globalThis.$) {
+    $.easing = Animation.easing
+    $.fn.extend({
+        animate: function(options){
+            return this.each(function(){
+                Animation.animate({
+                    el: this,
+                    ...options
+                }).then(() => {})
+            })
+        },
+
+        animationChain: function(arr, options){
+            return this.each(function(){
+                const el = this;
+                $.each(arr, function(){
+                    this.el = el;
+                });
+                Animation.chain(arr, options).then(() => {});
+            });
+        },
+
+        animationStop: function(done){
+            return this.each(function(){
+                const el = this;
+                $.each(Animation.elements, function(k, o){
+                    if (o.element === el) {
+                        Animation.stop(k, done);
+                    }
+                });
+            });
+        },
+
+        animationPause: function(){
+            return this.each(function(){
+                const el = this;
+                $.each(Animation.elements, function(k, o){
+                    if (o.element === el) {
+                        Animation.pause(k);
+                    }
+                });
+            });
+        },
+
+        animationResume: function(){
+            return this.each(function(){
+                const el = this;
+                $.each(Animation.elements, function(k, o){
+                    if (o.element === el) {
+                        Animation.resume(k);
+                    }
+                });
+            });
+        }
+    })
+}
